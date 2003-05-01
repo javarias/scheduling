0f4d8e58fcd68a69b8f52e2b9aab6d5176f0af9b
sroberts <sroberts@523d945c-050c-4681-91ec-863ad3bb968a>
2003-05-01 21:30:50 +0000

    update again!
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@16651 523d945c-050c-4681-91ec-863ad3bb968a

--- RealMode/test/testscript.sh
+++ RealMode/test/testscript.sh
@@ -3,6 +3,7 @@
 #javac -classpath $ACSROOT/lib/xmlstore.jar:$ACSROOT/lib/xmljbind.jar:$ACSROOT/lib/systementities.jar:$ACSROOT/lib/jcont.jar:$ACSROOT/lib/acsjlog.jar:$ACSROOT/lib/endorsed/xercesImpl.jar:$ACSROOT/lib/micarch.jar:$INTROOT/lib/ObsPrepEntities.jar:$ACSROOT/lib/xmlentity.jar alma/scheduling/test/SchedulingTestClient.java
 
 #TESTPATH=`pwd`
-TESTPATH=`cd ../lib`
+TESTPATH=`pwd`/../lib/schedulingTest.jar
+
 
 java -classpath $ACSROOT/lib/xmlstore.jar:$ACSROOT/lib/xmljbind.jar:$ACSROOT/lib/systementities.jar:$ACSROOT/lib/jcont.jar:$ACSROOT/lib/acsjlog.jar:$ACSROOT/lib/endorsed/xercesImpl.jar:$ACSROOT/lib/micarch.jar:$INTROOT/lib/ObsPrepEntities.jar:$ACSROOT/lib/xmlentity.jar:/alma/ACS-2.0/Orbacus/local/lib/OB.jar:$ACSROOT/lib/maci.jar:$TESTPATH alma.scheduling.test.SchedulingTestClient
