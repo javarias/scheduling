eaf8195918e4d9ae08cf9ed835002bbb54794945
sroberts <sroberts@523d945c-050c-4681-91ec-863ad3bb968a>
2003-05-01 20:09:24 +0000

    updated!
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@16649 523d945c-050c-4681-91ec-863ad3bb968a

--- RealMode/test/testscript.sh
+++ RealMode/test/testscript.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/ksh
 
-javac -classpath $ACSROOT/lib/xmlstore.jar:$ACSROOT/lib/xmljbind.jar:$ACSROOT/lib/systementities.jar:$ACSROOT/lib/jcont.jar:$ACSROOT/lib/acsjlog.jar:$ACSROOT/lib/endorsed/xercesImpl.jar:$ACSROOT/lib/micarch.jar:$INTROOT/lib/ObsPrepEntities.jar:$ACSROOT/lib/xmlentity.jar alma/scheduling/test/SchedulingTestClient.java
+#javac -classpath $ACSROOT/lib/xmlstore.jar:$ACSROOT/lib/xmljbind.jar:$ACSROOT/lib/systementities.jar:$ACSROOT/lib/jcont.jar:$ACSROOT/lib/acsjlog.jar:$ACSROOT/lib/endorsed/xercesImpl.jar:$ACSROOT/lib/micarch.jar:$INTROOT/lib/ObsPrepEntities.jar:$ACSROOT/lib/xmlentity.jar alma/scheduling/test/SchedulingTestClient.java
 
-TESTPATH=`pwd`
+#TESTPATH=`pwd`
+TESTPATH=`cd ../lib`
 
 java -classpath $ACSROOT/lib/xmlstore.jar:$ACSROOT/lib/xmljbind.jar:$ACSROOT/lib/systementities.jar:$ACSROOT/lib/jcont.jar:$ACSROOT/lib/acsjlog.jar:$ACSROOT/lib/endorsed/xercesImpl.jar:$ACSROOT/lib/micarch.jar:$INTROOT/lib/ObsPrepEntities.jar:$ACSROOT/lib/xmlentity.jar:/alma/ACS-2.0/Orbacus/local/lib/OB.jar:$ACSROOT/lib/maci.jar:$TESTPATH alma.scheduling.test.SchedulingTestClient
