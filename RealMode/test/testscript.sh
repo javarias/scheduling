09b43ebc6b34977fd8a872736ca28390897f7d0e
sroberts <sroberts@523d945c-050c-4681-91ec-863ad3bb968a>
2003-05-05 19:19:41 +0000

    updated testing script stuff
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@16688 523d945c-050c-4681-91ec-863ad3bb968a

--- RealMode/test/testscript.sh
+++ RealMode/test/testscript.sh
@@ -1,9 +1,30 @@
 #! /usr/bin/ksh
 
-#javac -classpath $ACSROOT/lib/xmlstore.jar:$ACSROOT/lib/xmljbind.jar:$ACSROOT/lib/systementities.jar:$ACSROOT/lib/jcont.jar:$ACSROOT/lib/acsjlog.jar:$ACSROOT/lib/endorsed/xercesImpl.jar:$ACSROOT/lib/micarch.jar:$INTROOT/lib/ObsPrepEntities.jar:$ACSROOT/lib/xmlentity.jar alma/scheduling/test/SchedulingTestClient.java
+# Get required paths
+ACS_CDB=`pwd`
 
-#TESTPATH=`pwd`
 TESTPATH=`pwd`/../lib/schedulingTest.jar
 
+# Start acs processes
+echo " Starting ORB Services"
+acsStartORBSRVC &
+sleep 50
+
+echo " Starting Manager"
+maciManager &
+sleep 15
+
+echo " Starting hugoActivator"
+acsStartContainer hugoActivator &
+sleep 10
+
+# Run test
+echo " Running test"
 
 java -classpath $ACSROOT/lib/xmlstore.jar:$ACSROOT/lib/xmljbind.jar:$ACSROOT/lib/systementities.jar:$ACSROOT/lib/jcont.jar:$ACSROOT/lib/acsjlog.jar:$ACSROOT/lib/endorsed/xercesImpl.jar:$ACSROOT/lib/micarch.jar:$INTROOT/lib/ObsPrepEntities.jar:$ACSROOT/lib/xmlentity.jar:/alma/ACS-2.0/Orbacus/local/lib/OB.jar:$ACSROOT/lib/maci.jar:$TESTPATH alma.scheduling.test.SchedulingTestClient
+
+# Shutdown acs and java processes
+sleep 20
+maciManagerShutdown &
+acsStopORBSRVC &
+killall java
