96ce09918a3f6e9bc19206c6b745ad1d0f06f60a
sroberts <sroberts@523d945c-050c-4681-91ec-863ad3bb968a>
2003-05-06 16:55:13 +0000

    updated the test again to properly kill hugo activator
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@16701 523d945c-050c-4681-91ec-863ad3bb968a

--- RealMode/test/testscript.sh
+++ RealMode/test/testscript.sh
@@ -24,7 +24,7 @@ echo " Running test"
 java -classpath $ACSROOT/lib/xmlstore.jar:$ACSROOT/lib/xmljbind.jar:$ACSROOT/lib/systementities.jar:$ACSROOT/lib/jcont.jar:$ACSROOT/lib/acsjlog.jar:$ACSROOT/lib/endorsed/xercesImpl.jar:$ACSROOT/lib/micarch.jar:$INTROOT/lib/ObsPrepEntities.jar:$ACSROOT/lib/xmlentity.jar:/alma/ACS-2.0/Orbacus/local/lib/OB.jar:$ACSROOT/lib/maci.jar:$TESTPATH alma.scheduling.test.SchedulingTestClient
 
 # Shutdown acs and java processes
-sleep 20
+maciActivatorShutdown hugoActivator &
 maciManagerShutdown &
 acsStopORBSRVC &
-killall java
+
