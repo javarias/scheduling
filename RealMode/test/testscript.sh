7736afec688becb78b818698af71b9e94e156ee7
sroberts <sroberts@523d945c-050c-4681-91ec-863ad3bb968a>
2003-05-07 17:51:19 +0000

    updated to have prologue and epilogue
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@16734 523d945c-050c-4681-91ec-863ad3bb968a

--- RealMode/test/testscript.sh
+++ RealMode/test/testscript.sh
@@ -1,30 +1,15 @@
-#! /usr/bin/ksh
-
-# Get required paths
-ACS_CDB=`pwd`
-
-TESTPATH=`pwd`/../lib/schedulingTest.jar
-
-# Start acs processes
-echo " Starting ORB Services"
-acsStartORBSRVC &
-sleep 50
-
-echo " Starting Manager"
-maciManager &
-sleep 15
+#! /bin/bash
 
 echo " Starting hugoActivator"
-acsStartContainer hugoActivator &
-sleep 10
+acsStartContainer hugoActivator&
+sleep 20
 
 # Run test
 echo " Running test"
 
-java -classpath $ACSROOT/lib/xmlstore.jar:$ACSROOT/lib/xmljbind.jar:$ACSROOT/lib/systementities.jar:$ACSROOT/lib/jcont.jar:$ACSROOT/lib/acsjlog.jar:$ACSROOT/lib/endorsed/xercesImpl.jar:$ACSROOT/lib/micarch.jar:$INTROOT/lib/ObsPrepEntities.jar:$ACSROOT/lib/xmlentity.jar:/alma/ACS-2.0/Orbacus/local/lib/OB.jar:$ACSROOT/lib/maci.jar:$TESTPATH alma.scheduling.test.SchedulingTestClient
 
-# Shutdown acs and java processes
-maciActivatorShutdown hugoActivator 
-maciManagerShutdown 
-acsStopORBSRVC 
+abeansStart -endorsed alma.scheduling.test.SchedulingTestClient
+
+
+maciActivatorShutdown hugoActivator >&  /dev/null
 
