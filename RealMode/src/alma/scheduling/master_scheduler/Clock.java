72b0cde2f3bb583f4e7e91b7f1cb47897fd97a2e
sroberts <sroberts@523d945c-050c-4681-91ec-863ad3bb968a>
2003-06-03 23:15:45 +0000

    changed some things to interfaces and deleted some files
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@17401 523d945c-050c-4681-91ec-863ad3bb968a

--- RealMode/src/alma/scheduling/master_scheduler/Clock.java
+++ RealMode/src/alma/scheduling/master_scheduler/Clock.java
@@ -1,17 +1,18 @@
-/**
- * ALMA - Atacama Large Millimiter Array
+/*
+ * ALMA - Atacama Large Millimeter Array
  * (c) European Southern Observatory, 2002
  * (c) Associated Universities Inc., 2002
  * Copyright by ESO (in the framework of the ALMA collaboration),
- * All rights reserved
+ * Copyright by AUI (in the framework of the ALMA collaboration),
+ * All rights reserved.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
+ * License as published by the Free software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  * 
  * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  * 
@@ -19,29 +20,53 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307  USA
- * 
- * File Clock.java
- * 
+ *
+ * File CLock.java
  */
+ 
 package alma.scheduling.master_scheduler;
 
-import alma.acs.container.ContainerServices;
-
 /**
- * The Clock class gives access to the array time, as implemented by
- * the Control System, in the real mode.  In the simulation mode, this
- * class accesses the ClockSimulator in the simulation class.
+ * Description 
  * 
- * @version 1.00 May 5, 2003
+ * @version 1.00  Jun 3, 2003
  * @author Allen Farris
  */
-public class Clock {
+public interface Clock {
 
-	public Clock (boolean isSimulation, ContainerServices container) {
-		System.out.println("The Clock has been constructed.");
-	}
+	/**
+	 * Return the current time as an STime object.  This time is
+	 * an approximation to the array time.  It uses the current system
+	 * time and adds a delta to it to synchronize it to the array time.
+	 * @return The current time as an STime object.
+	 */
+	public STime getSTime();
 
-	public static void main(String[] args) {
-	}
+	/**
+	 * Return the array time.  This time is from the control system,
+	 * if this is real; if it is a simulation, then it is from the
+	 * clock simulator.
+	 * @return The current time as an ArrayTime object.
+	 */
+	public ArrayTime getArrayTime();
+
+	/**
+	 * Synchronize the currrent system time and the array time.
+	 */
+	public void synchronize();
+
+	/**
+	 * Set an alarm, to go off at a particular time.
+	 * @param time		The time at which the alarm goes off.
+	 * @param listener	The listener method to call to process the alarm.
+	 */
+	public void setAlarm(STime time, ClockAlarmListener listener);
+
+	/**
+	 * Set an alarm, to go off at N seconds from now.
+	 * @param seconds	The number of seconds to wait for the alarm to go off.
+	 * @param listener	The listener method to call to process the alarm.
+	 */
+	public void setAlarm(int seconds, ClockAlarmListener listener);
+
 }
-
--- RealMode/src/alma/scheduling/master_scheduler/Dispatcher.java
+++ RealMode/src/alma/scheduling/master_scheduler/Dispatcher.java
@@ -1,17 +1,18 @@
-/**
- * ALMA - Atacama Large Millimiter Array
+/*
+ * ALMA - Atacama Large Millimeter Array
  * (c) European Southern Observatory, 2002
  * (c) Associated Universities Inc., 2002
  * Copyright by ESO (in the framework of the ALMA collaboration),
- * All rights reserved
+ * Copyright by AUI (in the framework of the ALMA collaboration),
+ * All rights reserved.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
+ * License as published by the Free software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  * 
  * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  * 
@@ -19,51 +20,25 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307  USA
- * 
+ *
  * File Dispatcher.java
- * 
  */
+ 
 package alma.scheduling.master_scheduler;
 
-//import alma.control.ArrayController;
-
-import alma.acs.container.ContainerServices;
-
 /**
- * The Dispatcher class presents a list of scheduling blocks for execution
- * to the telescope operator, in priority order, and waits for a selection.
- * If no selection is made, then the first one is selected.  It then 
- * initiates the execution of the selected scheduling block.
+ * Description 
  * 
- * @version 1.00 May 2, 2003
+ * @version 1.00  Jun 3, 2003
  * @author Allen Farris
  */
-public class Dispatcher {
-    private boolean isSimulation;
-    private ContainerServices containerServices;
-    //private Vector schedBlocks;
-    //private ArrayController ac;
-    
-	public Dispatcher (boolean isSimulation, ContainerServices container) {
-		System.out.println("The Dispatcher has been constructed.");
-        this.isSimulation = isSimulation;
-        this.containerServices = container;
-        //this.ac = new ArrayController();
-        
-	}
+public interface Dispatcher {
+	/**
+	 *  Tells the control system to execute the SB with 'id' and its start
+	 *  time is 'time'.
+	 *  @param id
+	 *  @param time
+	 */
+	public void sendToControl(String id, STime time);
 
-    /**
-     *  Tells the control system to execute the SB with 'id' and its start
-     *  time is 'time'.
-     *  @param id
-     *  @param time
-     */
-    public void sendToControl(String id, String time) {
-        System.out.println("Sending SB with id = "+id+" to controller.");
-        //ac.observe(id, time);
-    }
-
-	public static void main(String[] args) {
-	}
 }
-
