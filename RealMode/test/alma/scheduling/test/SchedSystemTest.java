66c5dc03861e32196bc73dec5d8510ac824f789f
sroberts <sroberts@523d945c-050c-4681-91ec-863ad3bb968a>
2003-09-11 14:49:29 +0000

    moved schedtest7 to schedsystemtest
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@19447 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ RealMode/test/alma/scheduling/test/SchedSystemTest.java
@@ -0,0 +1,209 @@
+/*
+ * ALMA - Atacama Large Millimiter Array
+ * (c) European Southern Observatory, 2002
+ * (c) Associated Universities Inc., 2002
+ * Copyright by ESO (in the framework of the ALMA collaboration),
+ * All rights reserved
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307  USA
+ * 
+ * File SchedSystemTest.java
+ * 
+ */
+package ALMA.scheduling.test;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import alma.xmlstore.*;
+import alma.xmlstore.OperationalPackage.*;
+import alma.xmlstore.CursorPackage.*;
+
+import alma.acs.container.ContainerServices;
+import alma.acs.container.ContainerException;
+import alma.acs.component.client.ComponentClient;
+
+import alma.acs.entityutil.EntitySerializer;
+import alma.acs.entityutil.EntityDeserializer;
+import alma.acs.entityutil.EntityException;
+
+import ALMA.scheduling.MS;
+import ALMA.scheduling.MSOperations;
+import ALMA.scheduling.MSPOATie;
+import ALMA.scheduling.NothingCanBeScheduledEvent;
+import ALMA.scheduling.NothingCanBeScheduledEventHelper;
+import ALMA.scheduling.master_scheduler.MSHelper;
+import ALMA.scheduling.InvalidOperation;
+import ALMA.scheduling.master_scheduler.MasterScheduler;
+import ALMA.scheduling.master_scheduler.ALMAArchive;
+import ALMA.scheduling.project_manager.ProjectManager;
+
+import alma.xmlentity.XmlEntityStruct;
+import alma.entity.xmlbinding.schedblock.SchedBlock;
+import alma.entity.xmlbinding.schedblock.SchedBlockEntityT;
+import alma.entity.xmlbinding.schedulingpolicy.SchedulingPolicy;
+import alma.entity.xmlbinding.schedulingpolicy.SchedulingPolicyEntityT;
+import alma.entity.xmlbinding.pipelineprocessingrequest.PipelineProcessingRequest;
+import alma.entity.xmlbinding.pipelineprocessingrequest.PipelineProcessingRequestEntityT;
+
+import ALMA.TelCalPublisher.PublishTelCalEvents;
+import ALMA.TelCalPublisher.PublishTelCalEventsHelper;
+
+import org.omg.CosNotification.*;
+import alma.acs.nc.*;
+
+/**
+ *  Test for R0+, Passing around a scheduling block!
+ *  @author Sohaila Roberts
+ */
+public class SchedSystemTest {
+    private EntitySerializer entitySerializer;
+    private EntityDeserializer entityDeserializer;
+    private ContainerServices containerServices;
+    private MS masterSchedulerComp;
+    private ProjectManager projectManager;
+    private SchedBlock[] sbs;
+    private Logger logger;
+    private TestSchedConsumer consumer;
+    private PublishTelCalEvents telcalComp;
+    
+    public SchedSystemTest(ContainerServices cs) {
+        this.containerServices = cs;
+        this.logger = cs.getLogger();
+        entitySerializer = EntitySerializer.getEntitySerializer(logger);
+        entityDeserializer = EntityDeserializer.getEntityDeserializer(logger);
+        // Get the MS interface as a component to test how the Exec will use it
+        try {
+            this.masterSchedulerComp = ALMA.scheduling.MSHelper.narrow(
+                containerServices.getComponent("MASTER_SCHEDULER"));
+            logger.fine("SCHED_TEST: MS Component created");
+        } catch(ContainerException e) {
+            logger.severe("SCHED_TEST: "+e.toString());
+        }
+    }
+    
+    /**
+     *  Calles the scheduling's startScheduling method using an
+     *  empty (newly created) SchedulingPolicy
+     */
+    public void testStartScheduling() {
+        logger.info("SCHED_TEST: About to call startScheduling");
+        try {
+            SchedulingPolicy sp = new SchedulingPolicy() ; 
+            SchedulingPolicyEntityT spe = new SchedulingPolicyEntityT();
+            try {
+                containerServices.assignUniqueEntityId(spe);
+                sp.setSchedulingPolicyEntity(spe);
+            } catch (ContainerException e) {
+                logger.severe("SCHED_TEST: ContainerException: "+ e.toString());
+            }
+            XmlEntityStruct sp_struct = entitySerializer.serializeEntity(sp);
+            masterSchedulerComp.startScheduling(sp_struct); 
+            logger.fine("SCHED_TEST: MasterScheduler startScheduling called.");
+        } catch(EntityException e) {
+            logger.fine("SCHED_TEST: EntityException: SchedulingPolicy not serialized!");
+            logger.severe("SCHED_TEST: EntityException: "+ e.toString());
+        } catch(InvalidOperation e) {
+            logger.severe("SCHED_TEST: InvalidOperation: startScheduling failed!");
+            logger.severe("SCHED_TEST: InvalidOperation: "+ e.toString());
+        }
+        logger.fine("SCHED_TEST: Scheduling started"); 
+        //testNothingToSchedule();
+        //testTelCalEvents();
+    }
+
+    public void testNothingToSchedule() {
+        consumer = new TestSchedConsumer(containerServices);
+        System.out.println("SCHED_TEST: SchedConsumer created");
+        try {
+            consumer.addSubscription(ALMA.acsnc.DEFAULTTYPE.value);
+            consumer.consumerReady();
+        } catch(Exception e) {
+            consumer.disconnect();
+            alma.acs.nc.Helper.disconnect();
+        }
+    }
+
+    public void testTelCalEvents() {
+        try{
+            telcalComp = ALMA.TelCalPublisher.PublishTelCalEventsHelper.narrow(
+                containerServices.getComponent("PUBLISHTELCALEVENTS1"));
+            logger.info("SCHED_TEST: TelCal Component Created!");
+
+            telcalComp.raisePointingScanReducedEvent("TestingPointing",1,2,3);
+            telcalComp.raiseFocusScanReducedEvent("TestingFocus",1,2,3);
+            
+        } catch (ContainerException e) {
+            logger.severe("SCHED_TEST: "+e.toString());
+        } catch(ALMA.ACSErr.ACSException e) {
+            logger.severe("SCHED_TEST: "+e.toString());
+        }
+    }
+
+    public void testGetStatus() {
+        boolean status = masterSchedulerComp.getStatus();
+        if(status) {
+            logger.info("SCHED_TEST: MasterScheduler's status = executing!");
+        } else {
+            logger.info("SCHED_TEST: MasterScheduler's status is not executing!");
+        }
+    }
+
+    public void testStopScheduling() {
+        try {
+            logger.info("SCHED_TEST: About to call stopScheduling");
+            masterSchedulerComp.stopScheduling();
+        } catch (InvalidOperation e) {
+            logger.severe("SCHED_TEST: InvalidOperation: "+ e.toString());
+        }
+    }
+    
+    public void release() {
+        logger.info("SCHED_TEST: About to release components");
+        containerServices.releaseComponent("MASTER_SCHEDULER");
+        containerServices.releaseComponent("PUBLISHTELCALEVENTS1");
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            String name = "Test7";
+            String managerLoc = System.getProperty("ACS.manager");
+            ComponentClient client = new ComponentClient(null, managerLoc, name);
+            ContainerServices cs = client.getContainerServices();
+            
+            SchedSystemTest test = new SchedSystemTest(cs);
+            
+            test.testStartScheduling();
+
+            test.testNothingToSchedule();
+
+            test.testTelCalEvents();
+            
+            Thread.sleep(1000*90);
+            
+            test.testGetStatus();
+            
+            test.testStopScheduling();
+
+            test.release();
+        } catch(Exception e) {
+            System.out.println("SCHED_TEST: Exception: " +e.toString() );
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+}
