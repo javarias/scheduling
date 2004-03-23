c919b7dfcc02cf2f5f35c0306827b56b86608f26
sroberts <sroberts@523d945c-050c-4681-91ec-863ad3bb968a>
2004-03-23 21:09:55 +0000

    *** empty log message ***
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@25019 523d945c-050c-4681-91ec-863ad3bb968a

--- PlanningModeSim/src/alma/scheduling/planning_mode_sim/gui/GUI.java
+++ PlanningModeSim/src/alma/scheduling/planning_mode_sim/gui/GUI.java
@@ -727,6 +727,7 @@ public class GUI extends JFrame {
             }
         });
         gridbag.setConstraints(tf,c);
+        tf.selectAll();
         header.add(tf);
         l = new JLabel();
         c.gridwidth = GridBagConstraints.REMAINDER;
