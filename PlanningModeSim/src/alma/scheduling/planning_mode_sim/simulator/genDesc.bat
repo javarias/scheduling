76393266ae6ea518df3d1ee674f07c7bdddccd24
sroberts <sroberts@523d945c-050c-4681-91ec-863ad3bb968a>
2004-08-10 16:04:53 +0000

    *** empty log message ***
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@28659 523d945c-050c-4681-91ec-863ad3bb968a

--- PlanningModeSim/src/alma/scheduling/planning_mode_sim/simulator/genDesc.bat
+++
@@ -1,1 +0,0 @@
-java -classpath d:\Users\afarris\projects\workspace\AFScheduling tools.ClassList d:\Users\afarris\projects\workspace\AFScheduling\ALMA\scheduling\simulator "The Scheduling Simulator Package"
