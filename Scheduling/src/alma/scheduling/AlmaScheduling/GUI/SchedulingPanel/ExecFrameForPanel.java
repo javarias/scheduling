d170e0b9d9216571108eee8e0992e0da074969a1
sslucero <sslucero@523d945c-050c-4681-91ec-863ad3bb968a>
2006-08-25 20:45:51 +0000

    exec plugin main class
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@57739 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ Scheduling/src/alma/scheduling/AlmaScheduling/GUI/SchedulingPanel/ExecFrameForPanel.java
@@ -0,0 +1,85 @@
+package alma.scheduling.AlmaScheduling.GUI.SchedulingPanel;
+
+//java stuff
+import java.awt.event.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JMenuBar;
+import javax.swing.JFrame;
+import java.util.logging.Logger;
+//acs stuff
+//import alma.acs.container.ContainerServices;
+//import alma.acs.component.client.ComponentClient;
+
+//exec plugin stuff
+import alma.exec.extension.subsystemplugin.SubsystemPlugin.*;
+
+public class ExecFrameForPanel extends JFrame implements SubsystemPlugin {
+//    private ContainerServices cs;
+    private PluginContainerServices cs;
+    private MainSchedTabPane mainSchedPanel;
+    private Logger logger;
+    
+    public ExecFrameForPanel(){
+        super("SchedulingPanel");
+        /*
+        int inset = 250;
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setBounds(inset, inset, screenSize.width - inset*2, 
+            screenSize.height - inset*2);
+        setSize(325, 600);
+        
+        JMenuBar menubar = new JMenuBar();
+        JMenu filemenu = new JMenu("File");
+        filemenu.setMnemonic(KeyEvent.VK_F);
+        JMenuItem exit = new JMenuItem("Exit", KeyEvent.VK_X);
+        exit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                exit();
+            }
+        });
+        filemenu.add(exit);
+        menubar.add(filemenu);
+        setJMenuBar(menubar);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                exit();
+            }
+        });
+
+        createMainSchedPanel(cs);
+        add(mainSchedPanel);
+        setVisible(true);
+        */
+    }
+
+    public void setServices(PluginContainerServices ctrl) {
+        cs = ctrl;
+        logger = ctrl.getLogger();
+    }
+
+    public void start() throws Exception {
+        createMainSchedPanel(cs);
+        add(mainSchedPanel);
+    }
+    public void stop() throws Exception{
+        exit();
+    }
+    public boolean runRestricted(boolean b) throws Exception {
+        return b;
+    }
+
+    public void exit(){
+        logger.info("Calling exit in ExecFrameForSchedulingPanel");
+        mainSchedPanel.exit();
+    }
+
+
+    private void createMainSchedPanel(ContainerServices cs) {
+        mainSchedPanel = new MainSchedTabPane(cs);
+    }
+
+}
