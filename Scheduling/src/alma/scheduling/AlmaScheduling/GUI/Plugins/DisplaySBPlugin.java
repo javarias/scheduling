c49e97108cb47e9a9f4087e6b9dc1796b6d585f7
sslucero <sslucero@523d945c-050c-4681-91ec-863ad3bb968a>
2006-08-29 22:55:33 +0000

    updated plugin
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@57828 523d945c-050c-4681-91ec-863ad3bb968a

--- Scheduling/src/alma/scheduling/AlmaScheduling/GUI/Plugins/DisplaySBPlugin.java
+++ Scheduling/src/alma/scheduling/AlmaScheduling/GUI/Plugins/DisplaySBPlugin.java
@@ -31,10 +31,13 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import java.awt.event.*;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.table.TableModel;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.border.TitledBorder;
@@ -47,18 +50,25 @@ import alma.scheduling.MasterSchedulerIF;
 /**
   * Plugin GUI for Exec 
   *
-  * @version $Id: DisplaySBPlugin.java,v 1.2 2006/08/29 17:14:38 sslucero Exp $
+  * @version $Id: DisplaySBPlugin.java,v 1.3 2006/08/29 22:55:33 sslucero Exp $
   */
 public class DisplaySBPlugin extends JPanel implements SubsystemPlugin {
 
+    private final String[] sbColumnInfo = {"SB Name","PI", "Project Name", "UID"};
+    private TableModel sbTableModel;
+    private JTable sbTable;
     private PluginContainerServices cs;
     private Logger logger;
     private MasterSchedulerIF ms = null;
+    private SBLite[] sblites=null;
     private Object[][] sbRowInfo;
-    private JTable sbTable;
     private JPanel eastPanel;
     private JPanel westPanel;
+    private JPanel detailsPanel;
+    private JTextArea detailsTA;
     
+    public DisplaySBPlugin (){}
+
     public void setServices(PluginContainerServices c) {
         this.cs = c;
         this.logger = cs.getLogger();
@@ -81,7 +91,9 @@ public class DisplaySBPlugin extends JPanel implements SubsystemPlugin {
 
     private void startPlugin() {
         try {
-            getMS();
+            if (cs != null){
+                getMS();
+            } 
             setup();
         } catch (Exception e) {
             e.printStackTrace(System.out);
@@ -101,18 +113,46 @@ public class DisplaySBPlugin extends JPanel implements SubsystemPlugin {
     }
 
     private void displaySbLites(){
-        final String[] sbColumnInfo = {"SB Name", "UID"};
         Dimension d = new Dimension(200,100);
         JPanel tablePanel = new JPanel();
         //populateSbRowInfo();
-        SBLite[] sblites= null;
-        sblites = ms.getSBLites();  
-        sbRowInfo = new Object[sblites.length][2];
+        sblites= null;
+        if (ms != null){
+            sblites = ms.getSBLites();  
+        } else {
+            sblites = new SBLite[1];
+            sblites[0] = new SBLite();
+            sblites[0].sbName = "foo";
+            sblites[0].PI = "foo's PI";
+            sblites[0].projectName = "foo's project";
+            sblites[0].schedBlockRef = "n/a";
+        }
+        sbRowInfo = new Object[sblites.length][sbColumnInfo.length];
         for(int i=0; i < sblites.length; i++){
             sbRowInfo[i][0] = sblites[i].sbName;
-            sbRowInfo[i][1] = sblites[i].schedBlockRef;
+            sbRowInfo[i][1] = sblites[i].PI;
+            sbRowInfo[i][2] = sblites[i].projectName;
+            sbRowInfo[i][3] = sblites[i].schedBlockRef;
         }
-        TableModel sbTableModel = new AbstractTableModel() {
+        createSBTableModel();
+        sbTable = new JTable(sbTableModel);
+        sbTable.addMouseListener(new MouseListener(){
+            public void mouseClicked(MouseEvent e) {
+                showSBInfo();
+            }
+            public void mouseEntered(MouseEvent e){ }
+            public void mouseExited(MouseEvent e){ }
+            public void mousePressed(MouseEvent e){ }
+            public void mouseReleased(MouseEvent e){}
+        });
+        sbTable.setPreferredScrollableViewportSize(d);
+        sbTable.doLayout();
+        JScrollPane pane = new JScrollPane(sbTable);
+        tablePanel.add(pane);
+        eastPanel.add(tablePanel);
+    }
+    private void createSBTableModel() {
+        sbTableModel= new AbstractTableModel() {
             public int getColumnCount() { return sbColumnInfo.length; }
             public String getColumnName(int col) { return sbColumnInfo[col]; }
             public int getRowCount() { return sbRowInfo.length; }
@@ -121,16 +161,12 @@ public class DisplaySBPlugin extends JPanel implements SubsystemPlugin {
             public void setValueAt(Object val, int row, int col) 
                 { sbRowInfo[row][col] = val; }
         };
-        sbTable = new JTable(sbTableModel);
-        sbTable.setPreferredScrollableViewportSize(d);
-        tablePanel.add(sbTable);
-        eastPanel.add(tablePanel);
     }
 
     private void displayDetails() {
-        JPanel p = new JPanel();
-        p.setBorder(new TitledBorder("SB Details"));
-        westPanel.add(p, BorderLayout.CENTER);
+        detailsPanel = new JPanel(new BorderLayout());
+        detailsPanel.setBorder(new TitledBorder("SB Details"));
+        westPanel.add(detailsPanel, BorderLayout.CENTER);
     }
 
     private void displayRefreshButton() {
@@ -155,6 +191,7 @@ public class DisplaySBPlugin extends JPanel implements SubsystemPlugin {
                             "IDL:alma/scheduling/MasterSchedulerIF:1.0"));
             }
         } catch(Exception e) {
+            ms = null;
             logger.severe("SCHED Plugin: Could not get master scheduler ref.");
             e.printStackTrace(System.out);
         }
@@ -171,24 +208,91 @@ public class DisplaySBPlugin extends JPanel implements SubsystemPlugin {
     }
     
     private void populateSbRowInfo() {
-        SBLite[] sblites= null;
-        try {
-            sblites = ms.getSBLites();  
-            sbRowInfo = new Object[sblites.length][2];
-            for(int i=0; i < sblites.length; i++){
-                sbRowInfo[i][0] = sblites[i].sbName;
-                sbRowInfo[i][1] = sblites[i].schedBlockRef;
-            }
-        }catch(Exception e){
-            e.printStackTrace();
+        sblites= null;
+        if(ms == null) {
+            sblites = new SBLite[2];
+            sblites[0] = new SBLite();
+            sblites[0].sbName = "foo";
+            sblites[0].PI = "foo's PI";
+            sblites[0].projectName = "foo's project";
+            sblites[0].schedBlockRef ="n/a";
+
+            sblites[1] = new SBLite();
+            sblites[1].sbName = "foo2";
+            sblites[1].PI = "foo2's PI";
+            sblites[1].projectName = "foo2's project";
+            sblites[1].schedBlockRef ="n/a";
+        } else {
+            try {
+                sblites = ms.getSBLites();  
+            }catch(Exception e){
+                e.printStackTrace();
+             }
+        }
+        sbRowInfo = new Object[sblites.length][sbColumnInfo.length];
+        for(int i=0; i < sblites.length; i++){
+            sbRowInfo[i][0] = sblites[i].sbName;
+            sbRowInfo[i][1] = sblites[i].PI;
+            sbRowInfo[i][2] = sblites[i].projectName;
+            sbRowInfo[i][3] = sblites[i].schedBlockRef;
         }
     }
 
     private void updateSBTable() {
         populateSbRowInfo();
-        sbTable.repaint();
+        System.out.println(sbRowInfo.length);
+        sbTable.revalidate();
         validate();
-        
+        System.out.println("should be updated now");
+    }
+
+    private void showSBInfo() {
+        try {//remove anything previously viewed in here
+            detailsPanel.removeAll();
+            detailsPanel.revalidate();
+            validate();
+        } catch(Exception e) { /* don't care if it complains! */ }
+        if(!checkOneSelected()) {
+            //show nothing
+            System.out.println("more than one");
+            return;
+        }
+        int row = sbTable.getSelectedRow();
+        SBLite sb=null;
+        for(int i=0; i < sblites.length; i++){
+            if(sblites[i].schedBlockRef.equals(sbRowInfo[row][3])){
+                sb = sblites[i];
+                break;
+            }
+        }
+        detailsTA = new JTextArea("Details:\n");
+        detailsTA.append("SB Name: "+ sb.sbName +"\n");
+        detailsTA.append("PI Name: "+sb.PI +"\n");
+        detailsPanel.add(detailsTA, BorderLayout.CENTER);
+        detailsPanel.revalidate();
+        validate();
+    }
+
+    private boolean checkOneSelected() {
+        int[] rows = sbTable.getSelectedRows();
+        if(rows.length > 1 ){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void main(String[] args){
+        try {
+            JFrame f = new JFrame();
+            DisplaySBPlugin foo = new DisplaySBPlugin();
+            foo.start();
+            f.add(foo);
+            f.pack();
+            f.setVisible(true);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
