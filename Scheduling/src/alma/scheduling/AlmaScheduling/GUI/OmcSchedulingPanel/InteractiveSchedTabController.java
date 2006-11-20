df9bfe72d82209d44d6bc219a3e012048694d3ef
sslucero <sslucero@523d945c-050c-4681-91ec-863ad3bb968a>
2006-11-20 03:40:17 +0000

    controller for IS tab
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@62575 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ Scheduling/src/alma/scheduling/AlmaScheduling/GUI/OmcSchedulingPanel/InteractiveSchedTabController.java
@@ -0,0 +1,127 @@
+package alma.scheduling.AlmaScheduling.GUI.OmcSchedulingPanel;
+
+import java.util.logging.Logger;
+import alma.scheduling.SBLite;
+import alma.scheduling.ProjectLite;
+import alma.scheduling.MasterSchedulerIF;
+import alma.scheduling.Interactive_PI_to_Scheduling;
+import alma.scheduling.Define.*;
+import alma.acs.nc.Consumer;
+import alma.Control.ExecBlockEndedEvent;
+import alma.offline.ASDMArchivedEvent;
+import alma.xmlstore.XmlStoreNotificationEvent;
+import alma.exec.extension.subsystemplugin.PluginContainerServices;
+
+public class InteractiveSchedTabController extends SchedulingPanelController {
+    private boolean sessionStarted;
+    private Interactive_PI_to_Scheduling scheduler;
+    private String schedulername;
+    private Consumer consumer = null;
+    private Consumer ctrl_consumer = null;
+    private String currentSBId;
+
+    
+    public InteractiveSchedTabController(PluginContainerServices cs, String name){
+        super(cs);
+        schedulername = name;
+    }
+
+    public void getISRef() {
+        try {
+            scheduler = alma.scheduling.Interactive_PI_to_SchedulingHelper.narrow(
+                    container.getComponent(schedulername));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        logger.info("Got interactive scheduler reference");
+
+    }
+    public void releaseISRef(){
+        try{
+            logger.info("About to release "+scheduler.name());
+            if(sessionStarted) {
+                getMSRef();
+                masterScheduler.stopInteractiveScheduler(schedulername);
+                releaseMSRef();
+            }
+            container.releaseComponent(schedulername);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void setSessionStarted(boolean b){
+        sessionStarted = b;
+    }
+    
+    public void executeSB(String id) throws SchedulingException {
+        try{
+            logger.info("IS: Sending sb ("+id+") to be executed");
+            scheduler.executeSB(id);
+        }catch( Exception e){
+            throw new SchedulingException (e);
+        }
+    }
+
+    public void stopSB()throws SchedulingException{
+        try{
+            logger.info("IS: Requesting sb to stop");
+            scheduler.stopSB();
+        }catch( Exception e){
+            throw new SchedulingException (e);
+        }
+    }
+
+    public void receive(XmlStoreNotificationEvent event) {
+        logger.info("IS: got xml update event");
+        CheckArchiveEvent processor = new CheckArchiveEvent(event);
+        Thread t = new Thread(processor);
+        t.start();
+    }
+
+    public void receive(ExecBlockEndedEvent e){
+        String exec_id = e.execId.entityId;
+        String sbid = e.sbId.entityId;
+        /*
+        boolean belongs = ebForThisScheduler(sbid);
+        if(belongs){
+            String completion;
+            switch(e.status.value()) {
+                case 0:
+                    completion ="FAILED";
+                    break;
+                case 1:
+                    completion ="SUCCESS";
+                    break;
+                case 2:
+                    completion ="PARTIAL";
+                    break;
+                case 3:
+                    completion ="TIMEOUT";
+                    break;
+                default:
+                    completion ="ERROR";
+                    break;
+            }*/
+          //  updateSBStatusInTable(sbid, completion);
+        //}
+    }
+    public void received(ASDMArchivedEvent e){
+        //ok to re-enable the search area now..
+    }
+
+    public void processXmlStoreNotificationEvent(XmlStoreNotificationEvent e) {
+    }
+
+    class CheckArchiveEvent implements Runnable {
+        private XmlStoreNotificationEvent event;
+
+        public CheckArchiveEvent(XmlStoreNotificationEvent e) {
+            event = e;
+        }
+        public void run(){
+            processXmlStoreNotificationEvent(event);
+        }
+    }
+}
