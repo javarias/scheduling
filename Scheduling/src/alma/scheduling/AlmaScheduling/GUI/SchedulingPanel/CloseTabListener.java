452fd6392ef6784de45d2ac13aae478f63d4fc54
sslucero <sslucero@523d945c-050c-4681-91ec-863ad3bb968a>
2007-02-06 15:29:02 +0000

    removed old scheduling panel files
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@65480 523d945c-050c-4681-91ec-863ad3bb968a

--- Scheduling/src/alma/scheduling/AlmaScheduling/GUI/SchedulingPanel/CloseTabListener.java
+++
@@ -1,9 +0,0 @@
-package alma.scheduling.AlmaScheduling.GUI.SchedulingPanel;
-
-import java.awt.event.MouseEvent;
-import java.util.EventListener;
-
-
-public interface CloseTabListener extends EventListener {
-	public void closeOperation(MouseEvent e);
-}
--- Scheduling/src/alma/scheduling/AlmaScheduling/GUI/SchedulingPanel/SchedulerTab.java
+++
@@ -1,8 +0,0 @@
-package alma.scheduling.AlmaScheduling.GUI.SchedulingPanel;
-
-public interface SchedulerTab{
-    public String getSchedulerName();
-    public String getArrayName();
-    public String getSchedulerType();
-    public void exit();
-}
