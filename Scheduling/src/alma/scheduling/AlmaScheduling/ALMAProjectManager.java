9a5aceab1ba764e7dca535084ae5274bd3d4a433
wlin <wlin@523d945c-050c-4681-91ec-863ad3bb968a>
2008-04-14 21:50:24 +0000

    check back to 1.103 cause the null point
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@88290 523d945c-050c-4681-91ec-863ad3bb968a

--- Scheduling/src/alma/scheduling/AlmaScheduling/ALMAProjectManager.java
+++ Scheduling/src/alma/scheduling/AlmaScheduling/ALMAProjectManager.java
@@ -72,7 +72,7 @@ import alma.xmlentity.XmlEntityStruct;
 /**
  *
  * @author Sohaila Lucero
- * @version $Id: ALMAProjectManager.java,v 1.105 2008/04/11 21:09:49 wlin Exp $
+ * @version $Id: ALMAProjectManager.java,v 1.106 2008/04/14 21:50:24 wlin Exp $
  */
 public class ALMAProjectManager extends ProjectManager {
     //The container services
@@ -685,20 +685,9 @@ public class ALMAProjectManager extends ProjectManager {
       * Gets called from ALMAReceiveEvent
       */
     public synchronized void createObservedSession(ExecBlock eb) {
-    	
-    	String sbid = eb.getParent().getId();
-    	// set sb and sb's parent status from ready to running 
-    	// this is specfic for manual mode array and IS/Queue/Dynamic will double set the status
-    	//will modify if scheduling receive the SessionEvent later
-    	SB sb = sbQueue.get(sbid);
-    	//logger.fine("SB ready time:"+sb.getStatus().getReadyTime());
-    	if((sb.getStatus().getStartTime() == null)){ 
-    		sb.setStartTime(clock.getDateTime());
-    		sb.setRunning();
-    	}
-    	//end of the sb status 
-    	
-        
+	
+	// start to add some code into fro manual mode
+        String sbid = eb.getParent().getId();
         Program p = ((SB)sbQueue.get(sbid)).getParent();
         ObservedSession session = new ObservedSession();
         session.setSessionId(eb.getSessionId());
