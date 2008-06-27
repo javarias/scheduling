ead1ad1137afefd48af92e65f4b2a2680cc1d7c4
wlin <wlin@523d945c-050c-4681-91ec-863ad3bb968a>
2008-06-27 20:08:47 +0000

    try to run more than one simulator SB
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@92211 523d945c-050c-4681-91ec-863ad3bb968a

--- Scheduling/test/alma/scheduling/test/TestSimulatorInteractiveSB.java
+++ Scheduling/test/alma/scheduling/test/TestSimulatorInteractiveSB.java
@@ -10,17 +10,20 @@ import alma.scheduling.MasterSchedulerIF;
 import alma.scheduling.AlmaScheduling.ALMAClock;
 import alma.scheduling.AlmaScheduling.*;
 import alma.scheduling.AlmaScheduling.ALMAMasterScheduler;
+import alma.scheduling.ProjectLite;
+import alma.scheduling.Interactive_PI_to_Scheduling;
+import alma.scheduling.AlmaScheduling.GUI.OmcSchedulingPanel.*;
+
 import alma.acs.component.client.ComponentClientTestCase;
 import alma.acs.component.client.ComponentClient;
 import alma.acs.container.ContainerServices;
 import alma.acs.component.client.AdvancedComponentClient;
-import java.util.logging.Logger;
 import alma.acs.logging.ClientLogManager;
-import alma.scheduling.AlmaScheduling.GUI.OmcSchedulingPanel.*;
-import alma.exec.extension.subsystemplugin.*;
-import alma.scheduling.ProjectLite;
-import alma.scheduling.Interactive_PI_to_Scheduling;
+import alma.ACS.MasterComponentPackage.SubsystemStateEvent;
 
+import alma.exec.extension.subsystemplugin.*;
+import alma.archive.ArchiveSubsystemMasterIF;
+import alma.pipelineql.QlDisplayMaster;
 /**
  * @author wlin
  *
@@ -35,6 +38,7 @@ public class TestSimulatorInteractiveSB {
 	private ContainerServices m_containerservices;
 	private Logger m_logger = null;
 	private Interactive_PI_to_Scheduling scheduler;
+	private Interactive_PI_to_Scheduling scheduler1;
 	private ALMAArchive archive;
 	
 	public TestSimulatorInteractiveSB() {
@@ -51,34 +55,78 @@ public class TestSimulatorInteractiveSB {
 		}
 		
 		try {
-			m_componentClient = new AdvancedComponentClient(m_logger,managerLoc,"SchedulingAlarmTestClient");
+			m_componentClient = new AdvancedComponentClient(m_logger,managerLoc,"SchedulingOMCTestClient");
 			if(m_componentClient==null)
 				System.exit(-1);
+			
 			m_containerservices = m_componentClient.getContainerServices();
 		    MasterSchedulerIF ms = alma.scheduling.MasterSchedulerIFHelper.narrow(
 		    		m_containerservices.getComponent("SCHEDULING_MASTERSCHEDULER"));
+		    
+		    //ArchiveSubsystemMasterIF masterArchive = alma.archive.ArchiveSubsystemMasterIFHelper.narrow(
+			//	m_containerservices.getComponent("ARCHIVE_MASTER_COMP"));
+		    
+		    //masterArchive.doTransition(SubsystemStateEvent.SUBSYSEVENT_INITPASS1);
+		    //masterArchive.doTransition(SubsystemStateEvent.SUBSYSEVENT_INITPASS2);
+		    //masterArchive.doTransition(SubsystemStateEvent.SUBSYSEVENT_START);
+		    
+		    
+		    //QlDisplayMaster qlDisplay=alma.pipelineql.QlDisplayMasterHelper.narrow(
+		    //		m_containerservices.getComponent("PIPELINE_QUICKLOOK_MASTER_COMP"));
+		    
+		    //archive.
 		    // create a array
 		    String[] antenna = {"DV01","DA41"};
+		    String[] array01 = {"DV01"};
+		    String[] array02 = {"DA41"};
 		    ms.createArray(antenna,ArrayModeEnum.INTERACTIVE);
+		    //ms.createArray(array02,ArrayModeEnum.INTERACTIVE);
 		    String schedulername = ms.startInteractiveScheduling1("Control/Array001");
+		    //String schedulername1 = ms.startInteractiveScheduling1("Control/Array002");
 		    scheduler = alma.scheduling.Interactive_PI_to_SchedulingHelper.narrow(
                     m_containerservices.getComponent(schedulername));
 		    
+		    //scheduler1 = alma.scheduling.Interactive_PI_to_SchedulingHelper.narrow(
+            //        m_containerservices.getComponent(schedulername1));
+	   	    for(int i=0;i<ms.getSBLites().length;i++) {
+ 
 			System.out.println("SB queue length:"+ms.getSBLites().length);
-			String id= (ms.getSBLites())[0].schedBlockRef;
+			//for(int i=0;i<ms.getSBLites().length;i++){
+			String id= (ms.getSBLites())[i].schedBlockRef;
 			System.out.println("id="+id);
-		        String currentSBId = id;
+			//String id1= (ms.getSBLites())[1].schedBlockRef;
+			//System.out.println("id1="+id1);
+			
+		    String currentSBId = id;
 		    System.out.println("get project for specified sbid");
 			ProjectLite project = ms.getProjectLiteForSB(id);
+			
+			//String currentSBId1 = id1;
+		    //System.out.println("get project for specified sbid");
+			//ProjectLite project1 = ms.getProjectLiteForSB(id1);
+			
 			System.out.println("start a session");
 			scheduler.startSession(project.piName,project.uid);
+			
+			//System.out.println("start a session");
+			//scheduler1.startSession(project1.piName,project1.uid);
+			
 			System.out.println("execute a sb");
 			scheduler.executeSB(id);
+			//System.out.println("execute a sb");
+			//scheduler1.executeSB(id1);
+			
 			System.out.println("set current id");
 			scheduler.setCurrentSB(id);	
+			
+			//System.out.println("set current id");
+			//scheduler1.setCurrentSB(id);	
+			
+			
+			}
 		} 
 		catch (Exception e ) {
-			System.out.println("Can not get SCHEDULING_MASTERSCHEDULER component");
+			System.out.println("Simulator Interactive SB fail");
 			System.out.println(e.toString());
 		}
 	}
@@ -95,7 +143,6 @@ public class TestSimulatorInteractiveSB {
 
 	public boolean runRestricted (boolean b) throws Exception {
 	    return b ;
-
 	}
 
 	/**
