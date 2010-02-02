cc319964ab4446ba24b90703ef7838b97378d505
ahoffsta <ahoffsta@523d945c-050c-4681-91ec-863ad3bb968a>
2010-02-02 21:35:00 +0000

    Adding license. Use of fail(() instead of throw() in tests case.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@124554 523d945c-050c-4681-91ec-863ad3bb968a

--- Common/test/alma/scheduling/output/OutputTest.java
+++ Common/test/alma/scheduling/output/OutputTest.java
@@ -1,3 +1,28 @@
+/*
+ * ALMA - Atacama Large Millimeter Array
+ * (c) European Southern Observatory, 2002
+ * (c) Associated Universities Inc., 2002
+ * Copyright by ESO (in the framework of the ALMA collaboration),
+ * Copyright by AUI (in the framework of the ALMA collaboration),
+ * All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307  USA
+ *
+ */
+
 package alma.scheduling.output;
 
 import java.io.FileNotFoundException;
@@ -67,11 +92,12 @@ public class OutputTest extends TestCase  {
                     new FileReader("./projects/outputTest01/output/output.xml"));
         } catch (MarshalException e) {
             e.printStackTrace();
-            throw e;
+        	fail("Unmarshalling problems. Please re-generate the castor classes.");
         } catch (ValidationException e) {
             e.printStackTrace();
-            throw e;
+            fail("XML validation failed. Please check your XML for syntax errors.");
         } catch (FileNotFoundException e) {
+        	fail("File not found, check for correct path and filename of the xml.");
             e.printStackTrace();
             throw e;
         }
