35d1e749a0883e02e350372fff38a0fecbccb768
ahoffsta <ahoffsta@523d945c-050c-4681-91ec-863ad3bb968a>
2010-02-04 23:05:05 +0000

    Added Executive data , obsprojects and starting the DSA.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@124769 523d945c-050c-4681-91ec-863ad3bb968a

--- DSA/test/alma/scheduling/test/ExecutiveRankerTest.java
+++ DSA/test/alma/scheduling/test/ExecutiveRankerTest.java
@@ -1,14 +1,33 @@
 package alma.scheduling.test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.ValidationException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import alma.scheduling.algorithm.DynamicSchedulingAlgorithm;
+import alma.scheduling.datamodel.BeanFactory;
+import alma.scheduling.datamodel.executive.Executive;
+import alma.scheduling.datamodel.executive.ExecutivePercentage;
+import alma.scheduling.datamodel.executive.ObservingSeason;
+import alma.scheduling.datamodel.executive.PI;
 import alma.scheduling.datamodel.obsproject.SchedBlock;
 import alma.scheduling.datamodel.obsproject.WeatherConstraints;
+import alma.scheduling.input.executive.generated.ExecutiveData;
 import alma.scheduling.persistence.HibernateUtil;
+import alma.scheduling.persistence.XmlUtil;
 import junit.framework.TestCase;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.xml.sax.SAXException;
 
 public class ExecutiveRankerTest extends TestCase {
 
@@ -31,6 +50,8 @@ public class ExecutiveRankerTest extends TestCase {
     }
  
     public void testExecutiveRanker() throws Exception {
+    	
+    	//Loading SchedBlocks
         Transaction tx = null;
         try {
             tx = session.beginTransaction();
@@ -50,6 +71,48 @@ public class ExecutiveRankerTest extends TestCase {
             tx.rollback();
             throw ex;
         }
+        
+        //Loading Executive data from XML
+        ExecutiveData execData = null;
+        try {
+            execData = ExecutiveData.unmarshalExecutiveData(
+                    new FileReader("./projects/SimpleDSATest01/executive/executive.xml"));
+        } catch (MarshalException e) {
+            e.printStackTrace();
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        //Loading Executive data from castor objects to hibernate
+        ArrayList<Executive> execOut =  new ArrayList<Executive>();
+        ArrayList<PI> piOut =  new ArrayList<PI>();
+        ArrayList<ExecutivePercentage> epOut =  new ArrayList<ExecutivePercentage>();
+        ArrayList<ObservingSeason> osOut = new ArrayList<ObservingSeason>();
+        
+        BeanFactory.copyExecutiveFromXMLGenerated(execData, execOut, piOut, epOut, osOut);
+        
+        try {
+            tx = session.beginTransaction();
+            for(ObservingSeason tmp: osOut)
+                session.save(tmp);
+            for(Executive tmp: execOut)
+                session.save(tmp);
+            for(PI tmp: piOut)
+                session.save(tmp);
+            tx.commit();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            tx.rollback();
+            throw ex;
+        }
+        
+        //Starting DSA
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
+		
+		DynamicSchedulingAlgorithm dsa = (DynamicSchedulingAlgorithm) ctx.getBean("dsa");
+		dsa.selectCandidateSB();
     }
     
 
