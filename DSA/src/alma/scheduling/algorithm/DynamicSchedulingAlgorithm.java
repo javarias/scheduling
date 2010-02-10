91c27f0cdd09c851c885f633618c4f4be7a78435
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2010-02-10 18:55:54 +0000

    Updated Test, it now works
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@125138 523d945c-050c-4681-91ec-863ad3bb968a

--- DSA/src/alma/scheduling/algorithm/DynamicSchedulingAlgorithm.java
+++ DSA/src/alma/scheduling/algorithm/DynamicSchedulingAlgorithm.java
@@ -1,22 +1,22 @@
 package alma.scheduling.algorithm;
 
-import java.util.Collection;
-
 import alma.scheduling.algorithm.sbselection.NoSbSelectedException;
-import alma.scheduling.algorithm.sbselection.SchedBlockSelector;
+import alma.scheduling.datamodel.obsproject.SchedBlock;
 
 public interface DynamicSchedulingAlgorithm {
 
     
-    public abstract void rankSchedBlocks();
+    public void rankSchedBlocks();
 
     /**
      * Clean the current candidate SBs and run again the selectors
      * @throws NoSbSelectedException if a selector cannot get SBs or if this method
      * cannot intersect a common group between al SBs returned by the selectors used
      */
-    public abstract void selectCandidateSB() throws NoSbSelectedException;
+    public void selectCandidateSB() throws NoSbSelectedException;
 
-    public abstract void updateModel();
+    public void updateModel();
+    
+    public SchedBlock getSelectedSchedBlock();
 
 }
