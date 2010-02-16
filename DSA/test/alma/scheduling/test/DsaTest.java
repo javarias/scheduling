a4eae0a8a26d0960f63f460f6522a5cbfdacf8b8
rhiriart <rhiriart@523d945c-050c-4681-91ec-863ad3bb968a>
2010-02-16 18:11:18 +0000

    Correcting typo.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@125478 523d945c-050c-4681-91ec-863ad3bb968a

--- DSA/test/alma/scheduling/test/DsaTest.java
+++ DSA/test/alma/scheduling/test/DsaTest.java
@@ -5,12 +5,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import alma.scheduling.algorithm.DynamicSchedulingAlgorithm;
-import alma.scheduling.algorithm.sbselection.NoSbSelectedExecption;
+import alma.scheduling.algorithm.sbselection.NoSbSelectedException;
 
 public class DsaTest {
 	
 	
-	public static void main (String args[]) throws NoSbSelectedExecption{
+	public static void main (String args[]) throws NoSbSelectedException{
 		
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
 		
