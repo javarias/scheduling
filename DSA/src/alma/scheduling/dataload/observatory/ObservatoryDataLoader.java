b9573920b54e5419fb81062e26f4f06b3c503345
rhiriart <rhiriart@523d945c-050c-4681-91ec-863ad3bb968a>
2010-03-12 02:21:28 +0000

    Implementing the Observatory Characteristics data loader.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@127145 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ DSA/src/alma/scheduling/dataload/observatory/ObservatoryDataLoader.java
@@ -0,0 +1,61 @@
+/*
+ * ALMA - Atacama Large Millimeter Array
+ * (c) European Southern Observatory, 2002
+ * (c) Associated Universities Inc., 2002
+ * Copyright by ESO (in the framework of the ALMA collaboration),
+ * Copyright by AUI (in the framework of the ALMA collaboration),
+ * All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307  USA
+ *
+ * "@(#) $Id: ObservatoryDataLoader.java,v 1.1 2010/03/12 02:21:28 rhiriart Exp $"
+ */
+package alma.scheduling.dataload.observatory;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import alma.scheduling.dataload.DataLoader;
+import alma.scheduling.datamodel.observatory.ArrayConfiguration;
+import alma.scheduling.datamodel.observatory.TelescopeEquipment;
+import alma.scheduling.datamodel.observatory.dao.ObservatoryDao;
+import alma.scheduling.datamodel.observatory.dao.XmlObservatoryDao;
+
+@Transactional
+public class ObservatoryDataLoader implements DataLoader {
+
+    XmlObservatoryDao xmlDao;
+    
+    ObservatoryDao dao;
+    
+    public void setXmlDao(XmlObservatoryDao xmlDao) {
+        this.xmlDao = xmlDao;
+    }
+
+    public void setDao(ObservatoryDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public void load() {
+        List<TelescopeEquipment> equipments = xmlDao.getAllEquipments();
+        dao.saveOrUpdate(equipments);
+        List<ArrayConfiguration> arrConfigs = xmlDao.getAllArrayConfigurations();
+        dao.saveOrUpdate(arrConfigs);
+    }
+
+}
