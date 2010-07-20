55a7488fdf436b61f1d94f756ce2e0ddbbafcfcb
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2010-07-20 21:41:50 +0000

    Added In-Memory Weather DAO
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@135815 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ Common/src/alma/scheduling/datamodel/weather/dao/MemoryWeatherHistoryDAOImpl.java
@@ -0,0 +1,60 @@
+package alma.scheduling.datamodel.weather.dao;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import alma.scheduling.datamodel.weather.TemperatureHistRecord;
+
+public class MemoryWeatherHistoryDAOImpl extends WeatherHistoryDAOImpl
+        implements WeatherHistoryDAO {
+
+    private static Logger logger = LoggerFactory.getLogger(MemoryWeatherHistoryDAOImpl.class);
+    
+    private TemperatureHistRecord cache[] = null;
+    private Long startTime = null;
+    private Double maxTime = null;
+
+    @Override
+    public TemperatureHistRecord getTemperatureForTime(Date ut) {
+        //Cache init
+        if(cache == null)
+            fillCache();
+        if(startTime == null)
+            startTime = getSimulationStartTime().getTime();
+        if(maxTime == null){
+            Query query;
+            query = getSession().getNamedQuery("TemperatureHistRecord.getMaxTime");
+            maxTime = (Double) query.uniqueResult();
+            logger.info("max time in temperature historical records: " + maxTime);
+        }
+        double dt = ( ut.getTime() - getSimulationStartTime().getTime() ) / ( 3600000.0); // difference in time (hours)
+        dt = dt % maxTime;
+        int pos = (int) (dt / 0.010416667 + 0.1);
+        System.out.println(pos);
+        
+        Double temperature = cache[pos].getValue();
+        if (temperature < -500) {
+            logger.info("lower bound not a valid value, looking at the next 5 values");
+            for (int i = 0; i < 5 ; i++) {
+                TemperatureHistRecord t = cache[pos + i];
+                if (t.getValue() > -500) {
+                    return t;
+                }
+            }
+        }
+        return cache[pos];
+    }
+    
+    private void fillCache(){
+        cache =  new TemperatureHistRecord[35065];
+        List<TemperatureHistRecord> tmp = findAllOrdered();
+        for(int i = 0; i < cache.length; i++){
+            cache[i] = tmp.get(i);
+        }
+    }
+}
