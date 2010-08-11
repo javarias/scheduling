e1e7e05b360f16cb8489921ae964b7170ab15293
ahoffsta <ahoffsta@523d945c-050c-4681-91ec-863ad3bb968a>
2010-08-11 23:12:21 +0000

    Adding new thread runner for simulation
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@136918 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ PlanningModeSim/src/alma/scheduling/psm/sim/SimulatorThread.java
@@ -0,0 +1,52 @@
+package alma.scheduling.psm.sim;
+
+import java.util.Date;
+
+import alma.scheduling.algorithm.VerboseLevel;
+import alma.scheduling.psm.sim.Simulator;
+
+public class SimulatorThread extends Thread{
+	
+	private Date currentDate;
+	private Date startDate;
+	private Date stopDate;
+
+	private VerboseLevel verboseLvl;
+	private String workDir;
+	
+	public SimulatorThread( String workDir, VerboseLevel verboseLevel ){
+		this.workDir = workDir;
+		this.verboseLvl = verboseLevel;
+	}
+
+	synchronized public Date getCurrentDate() {
+		return currentDate;
+	}
+
+	synchronized public void setCurrentDate(Date currentDate) {
+		this.currentDate = currentDate;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	
+	public Date getStopDate() {
+		return stopDate;
+	}
+
+	public void setStopDate(Date stopDate) {
+		this.stopDate = stopDate;
+	}
+
+	public void run(){
+		Simulator simulator = new Simulator( workDir, this );
+    	simulator.setVerboseLvl(verboseLvl);
+    	simulator.run();
+	}
+
+}
