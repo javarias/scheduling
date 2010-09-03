ae7bfca0c02d7701e27261ea7ea74a6bfabcd3c6
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2010-09-03 21:59:57 +0000

    Added automatically creation of executives
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@138796 523d945c-050c-4681-91ec-863ad3bb968a

--- Common/src/alma/scheduling/datamodel/executive/dao/ExecutiveDaoImpl.java
+++ Common/src/alma/scheduling/datamodel/executive/dao/ExecutiveDaoImpl.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307  USA
  *
- * "@(#) $Id: ExecutiveDaoImpl.java,v 1.22 2010/09/03 21:48:45 javarias Exp $"
+ * "@(#) $Id: ExecutiveDaoImpl.java,v 1.23 2010/09/03 21:59:57 javarias Exp $"
  */
 package alma.scheduling.datamodel.executive.dao;
 
@@ -92,18 +92,6 @@ public class ExecutiveDaoImpl extends GenericDaoImpl implements ExecutiveDAO {
     @Transactional(readOnly=true)
     public Executive getExecutive(String piEmail) {
         PI pi = findById(PI.class, piEmail);
-        if(pi == null){
-            pi = new PI();
-            pi.setEmail(piEmail);
-            pi.setEmail(piEmail);
-            pi.setPIMembership(new HashSet<PIMembership>());
-            PIMembership pim = new PIMembership();
-            pim.setExecutive(getAllExecutive().get(0));
-            pim.setMembershipPercentage(1);
-            pi.getPIMembership().add(pim);
-            System.out.println("WARNING: Adding new PI: sb.getPiName()");
-            saveOrUpdate(pi);
-        }
         Executive exec = pi.getPIMembership().iterator().next().getExecutive();
         for (ExecutivePercentage ep : exec.getExecutivePercentage()) {
             ObservingSeason o = ep.getSeason();
@@ -118,6 +106,18 @@ public class ExecutiveDaoImpl extends GenericDaoImpl implements ExecutiveDAO {
         Query query = getSession().createQuery("from PI as p where p.email = ?");
         query.setParameter(0, piEmail);
         PI pi = (PI) query.uniqueResult();
+        if(pi == null){
+            pi = new PI();
+            pi.setName(piEmail);
+            pi.setEmail(piEmail);
+            pi.setPIMembership(new HashSet<PIMembership>());
+            PIMembership pim = new PIMembership();
+            pim.setExecutive(getAllExecutive().get(0));
+            pim.setMembershipPercentage(1);
+            pi.getPIMembership().add(pim);
+            System.out.println("WARNING: Adding new PI: sb.getPiName()");
+            saveOrUpdate(pi);
+        }
         for( PIMembership pim : pi.getPIMembership() ){
         	pim.getMembershipPercentage();
         }
