be06ba68dec658db1914863feec509c7dae49219
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2010-09-10 23:29:25 +0000

    Added ProjectQualitySelector to discard D class projects and SBs
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@139340 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ DSA/src/alma/scheduling/algorithm/obsproject/ProjectQualitySelector.java
@@ -0,0 +1,35 @@
+package alma.scheduling.algorithm.obsproject;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+
+import alma.scheduling.algorithm.sbselection.AbstractBaseSelector;
+import alma.scheduling.algorithm.sbselection.NoSbSelectedException;
+import alma.scheduling.datamodel.observatory.ArrayConfiguration;
+import alma.scheduling.datamodel.obsproject.SchedBlock;
+import alma.scheduling.datamodel.obsproject.SchedBlockState;
+import alma.scheduling.datamodel.obsproject.ScienceGrade;
+
+public class ProjectQualitySelector extends AbstractBaseSelector {
+
+    public ProjectQualitySelector(String selectorName) {
+        super(selectorName);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Criterion getCriterion(Date ut, ArrayConfiguration arrConf) {
+        Criterion crit = Restrictions.ne("letterGrade", ScienceGrade.D);
+        return crit;
+    }
+
+    @Override
+    public Collection<SchedBlock> select(Date ut, ArrayConfiguration arrConf)
+            throws NoSbSelectedException {
+        return null;
+    }
+
+}
