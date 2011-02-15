a166eb56269b9b0316199858e4cee20c07c45bed
ahoffsta <ahoffsta@523d945c-050c-4681-91ec-863ad3bb968a>
2011-02-15 13:34:36 +0000

    New data bean for report generation
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@148042 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ PlanningModeSim/src/alma/scheduling/psm/reports/domain/ObsProjectReportBean.java
@@ -0,0 +1,78 @@
+package alma.scheduling.psm.reports.domain;
+
+public class ObsProjectReportBean {
+	
+	private String band;
+	private String executive;
+	private String lstRange;
+	private double executionTime;
+	private String grade;
+	private long scienceRank;
+	private double scienceScore;
+	
+	public static double totalExecutionTime;
+
+	public String getBand() {
+		return band;
+	}
+
+	public void setBand(String band) {
+		this.band = band;
+	}
+
+	public String getExecutive() {
+		return executive;
+	}
+
+	public void setExecutive(String executive) {
+		this.executive = executive;
+	}
+
+	public double getExecutionTime() {
+		return executionTime;
+	}
+
+	public void setExecutionTime(double executionTime) {
+		this.executionTime = executionTime;
+	}
+	
+	public String getLstRange() {
+		return lstRange;
+	}
+
+	public void setLstRange(String lstRange) {
+		this.lstRange = lstRange;
+	}
+
+	public void setTotalExecutionTime(double totalExecutionTime) {
+		SchedBlockReportBean.totalExecutionTime = totalExecutionTime;
+	}
+
+	public double getTotalExecutionTime() {
+		return totalExecutionTime;
+	}
+
+	public String getGrade(){
+		return grade;
+	}
+
+	public void setGrade(String grade){
+		this.grade = grade;
+	}
+
+	public long getScienceRank(){
+		return this.scienceRank;
+	}
+
+	public void setScienceRank( long scienceRank ){
+		this.scienceRank = scienceRank;
+	}
+
+	public double getScienceScore(){
+		return this.scienceScore;
+	}
+
+	public void setScienceScore( double scienceScore ){
+		this.scienceScore = scienceScore;
+	}
+}
