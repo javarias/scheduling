0af636f37f94a379f59fd652c54b1dbcc9427a9a
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2011-03-21 22:07:53 +0000

    Completed results dao
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@150515 523d945c-050c-4681-91ec-863ad3bb968a

--- DSA/src/alma/scheduling/algorithm/results/dao/ResultsDaoImpl.java
+++ DSA/src/alma/scheduling/algorithm/results/dao/ResultsDaoImpl.java
@@ -1,7 +1,6 @@
 package alma.scheduling.algorithm.results.dao;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.hibernate.Query;
 
 import alma.scheduling.algorithm.results.Result;
 import alma.scheduling.datamodel.GenericDaoImpl;
@@ -13,25 +12,35 @@ import alma.scheduling.datamodel.GenericDaoImpl;
  * 
  * @since ALMA 8.1.0
  * @author javarias
- * $Id: ResultsDaoImpl.java,v 1.3 2011/03/17 22:58:35 javarias Exp $
+ * $Id: ResultsDaoImpl.java,v 1.4 2011/03/21 22:07:53 javarias Exp $
  */
 public class ResultsDaoImpl extends GenericDaoImpl implements ResultsDao {
 
 	@Override
 	public void saveOrUpdate(Result result) {
+		Query query = getSession().createQuery("select res from Result where res.array = ? order by res.time desc");
+		query.setParameter(0, result.getArrayName());
+		try {
+			Result old = (Result) query.list().get(1);
+			delete(old);
+		} catch (IndexOutOfBoundsException ex) {
+			//Do nothing there are no old results
+		}
 		saveOrUpdate(result);
 	}
 
 	@Override
 	public Result getCurrentResult(String arrayName) {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = getSession().createQuery("select res from Result where res.array = ? order by res.time desc");
+		query.setParameter(0, arrayName);
+		return (Result) query.list().get(0);
 	}
 
 	@Override
 	public Result getPreviousResult(String arrayName) {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = getSession().createQuery("select res from Result where res.array = ? order by res.time desc");
+		query.setParameter(0, arrayName);
+		return (Result) query.list().get(1);
 	}
 
 }
