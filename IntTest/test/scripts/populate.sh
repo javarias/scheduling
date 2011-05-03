da470943d035fb2361248360f7719eccce0f1141
dclarke <dclarke@523d945c-050c-4681-91ec-863ad3bb968a>
2011-05-03 21:03:35 +0000

    Go looking for the script rather than making assumptions about where it is
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@154751 523d945c-050c-4681-91ec-863ad3bb968a

--- IntTest/test/scripts/populate.sh
+++ IntTest/test/scripts/populate.sh
@@ -7,10 +7,21 @@ else
    projects=($@)
 fi
 
+basicScript="import-saveasnew.py"
+
+script=`which ${basicScript}`
+echo $script
+
+if [ -z "${script}" ]
+then
+   echo "Cannot find script ${basicScript} on path"
+   exit 666
+fi
+
 export JAVA_OPTIONS="-Dot.submission.host=http://127.0.0.1:8180 -Dot.submission.user=john -Dot.submission.pass=john -Dalma.obsprep.allowprivileged=true"
 
 for project in ${projects[@]} ; do
-    ALMA-OT -batch $INTROOT/bin/import-saveasnew.py ${project}
+    ALMA-OT -batch ${script} ${project}
 done
 
 unset JAVA_OPTIONS
