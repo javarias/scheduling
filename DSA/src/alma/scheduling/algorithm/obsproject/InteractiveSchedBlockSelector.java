9615eef36b62612151349a9452f9fcc8588353f6
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2011-06-22 22:54:20 +0000

    Added Selector, to select only the interactive SBs
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@157700 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ DSA/src/alma/scheduling/algorithm/obsproject/InteractiveSchedBlockSelector.java
@@ -0,0 +1,31 @@
+package alma.scheduling.algorithm.obsproject;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+
+import alma.scheduling.algorithm.sbselection.AbstractBaseSelector;
+import alma.scheduling.algorithm.sbselection.NoSbSelectedException;
+import alma.scheduling.datamodel.observatory.ArrayConfiguration;
+import alma.scheduling.datamodel.obsproject.SchedBlock;
+
+public class InteractiveSchedBlockSelector extends AbstractBaseSelector {
+
+	public InteractiveSchedBlockSelector(String selectorName) {
+		super(selectorName);
+	}
+
+	@Override
+	public Collection<SchedBlock> select(Date ut, ArrayConfiguration arrConf)
+			throws NoSbSelectedException {
+		return null;
+	}
+
+	@Override
+	public Criterion getCriterion(Date ut, ArrayConfiguration arrConf) {
+		return Restrictions.not(Restrictions.eq("s.manual", false));
+	}
+
+}
