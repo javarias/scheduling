f0a0820fdc0218334572eb5a0536007d3e7c14bb
dclarke <dclarke@523d945c-050c-4681-91ec-863ad3bb968a>
2011-07-13 21:50:41 +0000

    Added logging of key information to operator
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@158620 523d945c-050c-4681-91ec-863ad3bb968a

--- Master/src/alma/scheduling/master/compimpl/MasterImpl.java
+++ Master/src/alma/scheduling/master/compimpl/MasterImpl.java
@@ -21,9 +21,17 @@ package alma.scheduling.master.compimpl;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Logger;
 
+import org.omg.CORBA.Context;
+import org.omg.CORBA.ContextList;
+import org.omg.CORBA.DomainManager;
+import org.omg.CORBA.ExceptionList;
+import org.omg.CORBA.NVList;
+import org.omg.CORBA.NamedValue;
 import org.omg.CORBA.Object;
+import org.omg.CORBA.Policy;
+import org.omg.CORBA.Request;
+import org.omg.CORBA.SetOverrideType;
 
 import alma.ACS.ComponentStates;
 import alma.Control.ArrayIdentifier;
@@ -35,6 +43,8 @@ import alma.Control.CurrentWeatherHelper;
 import alma.Control.InaccessibleException;
 import alma.Control.InvalidRequest;
 import alma.JavaContainerError.wrappers.AcsJContainerServicesEx;
+import alma.SchedulingArrayExceptions.NoRunningSchedBlockEx;
+import alma.SchedulingExceptions.InvalidOperationEx;
 import alma.SchedulingMasterExceptions.ACSInternalExceptionEx;
 import alma.SchedulingMasterExceptions.ArrayNotFoundExceptionEx;
 import alma.SchedulingMasterExceptions.ControlInternalExceptionEx;
@@ -47,30 +57,54 @@ import alma.acs.component.ComponentDescriptor;
 import alma.acs.component.ComponentLifecycle;
 import alma.acs.component.ComponentQueryDescriptor;
 import alma.acs.container.ContainerServices;
+import alma.acs.logging.AcsLogger;
+import alma.acs.logging.domainspecific.AudienceLogger;
+import alma.acs.logging.domainspecific.AudienceLogger.Audience;
+import alma.asdmIDLTypes.IDLEntityRef;
 import alma.scheduling.Array;
 import alma.scheduling.ArrayCreationInfo;
 import alma.scheduling.ArrayEvent;
+import alma.scheduling.ArrayGUICallback;
 import alma.scheduling.ArrayHelper;
 import alma.scheduling.ArrayModeEnum;
 import alma.scheduling.ArraySchedulerLifecycleType;
 import alma.scheduling.ArraySchedulerMode;
 import alma.scheduling.ArrayStatusCallback;
 import alma.scheduling.MasterOperations;
+import alma.scheduling.SchedBlockExecutionCallback;
+import alma.scheduling.SchedBlockExecutionItem;
+import alma.scheduling.SchedBlockQueueCallback;
+import alma.scheduling.SchedBlockQueueItem;
 import alma.scheduling.array.util.NameTranslator;
 import alma.scheduling.array.util.NameTranslator.TranslationException;
 import alma.scheduling.datamodel.weather.dao.WeatherStationDao;
+import alma.scheduling.utils.AudienceFlogger;
 import alma.scheduling.utils.DSAContextFactory;
 import alma.scheduling.utils.DynamicSchedulingPolicyFactory;
+import alma.scheduling.utils.FakeAudienceFlogger;
 
 public class MasterImpl implements ComponentLifecycle,
         MasterOperations {
 
     private ContainerServices m_containerServices;
-    private Logger m_logger;
+    private AcsLogger m_logger;
+    private AudienceFlogger operatorLog;
     private ControlMaster controlMaster;
     private HashMap<String, ArrayModeEnum> activeArrays;
     private HashMap<String, ArrayStatusCallback> callbacks;
     private CurrentWeather weatherComp;
+	
+    /////////////////////////////////////////////////////////////
+    // Logging
+    /////////////////////////////////////////////////////////////
+ 
+    private void initialiseLoggers() {
+    	if (m_logger == null || operatorLog == null) {
+        	m_logger = m_containerServices.getLogger();
+        	operatorLog = new AudienceFlogger(m_logger,
+        			AudienceLogger.Audience.OPERATOR);
+    	}
+    }
 	
     /////////////////////////////////////////////////////////////
     // Implementation of ComponentLifecycle
@@ -83,23 +117,25 @@ public class MasterImpl implements ComponentLifecycle,
     
     public void initialize(ContainerServices containerServices) {
         m_containerServices = containerServices;
-        if (m_logger == null)
-        	m_logger = m_containerServices.getLogger();
+        initialiseLoggers();
         
         //Get Weather Station reference and pass it to Weather Station Dao.
         //Change this if Master Scheduler is not longer deployed in the same container
         //than the SCHEDULING Arrays
         try {
-			weatherComp = CurrentWeatherHelper.narrow(containerServices.getDefaultComponent("IDL:alma/Control/CurrentWeather:1.0"));
+			weatherComp = CurrentWeatherHelper.narrow(containerServices.getDefaultComponent(Constants.WEATHER_STATION_IF));
 			String nameComp = weatherComp.name();
 			containerServices.releaseComponent(nameComp);
 			weatherComp = null;
 			weatherComp = CurrentWeatherHelper.narrow(containerServices.getComponentNonSticky(nameComp));
 			WeatherStationDao.setWeatherStation(weatherComp);
+			operatorLog.info("Connected to Weather Station Controller Component");
 		} catch (AcsJContainerServicesEx e) {
 			m_logger.warning("Unable to retrieve Weather Station Controller Component: IDL:alma/Control/CurrentWeather:1.0");
+			operatorLog.warning("Unable to retrieve Weather Station Controller Component %s", Constants.WEATHER_STATION_IF);
 		} catch (org.omg.CORBA.SystemException e) {
 			m_logger.warning("Unable to retrieve Weather Station Controller Component: IDL:alma/Control/CurrentWeather:1.0");
+			operatorLog.warning("Unable to retrieve Weather Station Controller Component %s", Constants.WEATHER_STATION_IF);
 		}
 		m_logger.finest("initialize() called...");
     }
@@ -110,23 +146,28 @@ public class MasterImpl implements ComponentLifecycle,
 
     public void cleanUp() {
     	for(String arrayName: activeArrays.keySet()){
-    		m_logger.warning("Triying to destroy Array: " + arrayName + " at cleanUp. " +
-    				"You should destroy the arrays before component cleanUp");
+    		operatorLog.warning("Automatically destroying %s at cleanUp. " +
+				"You should destroy the arrays before component cleanUp", arrayName);
+    		m_logger.warning("Automatically destroying " + arrayName + " at cleanUp.");
     		try {
 				destroyArray(arrayName, "Clean-up", "");
 				m_logger.fine("Array: " + arrayName + " Destroyed successfully");
+				operatorLog.info("%s destroyed", arrayName);
 			} catch (ACSInternalExceptionEx e) {
 				m_logger.severe("Array: " + arrayName + " Cannot be destroyed");
 				AcsJSchedulingInternalExceptionEx ex = new AcsJSchedulingInternalExceptionEx(e); 
 				ex.log(m_logger);
+				operatorLog.warning("Cannot destroy %s - ACS internal error", arrayName);
 			} catch (ControlInternalExceptionEx e) {
 				m_logger.severe("Array: " + arrayName + " Cannot be destroyed");
 				AcsJSchedulingInternalExceptionEx ex = new AcsJSchedulingInternalExceptionEx(e); 
 				ex.log(m_logger);
+				operatorLog.warning("Cannot destroy %s - Control subsystem internal error", arrayName);
 			} catch (SchedulingInternalExceptionEx e) {
 				m_logger.severe("Array: " + arrayName + " Cannot be destroyed");
 				AcsJSchedulingInternalExceptionEx ex = new AcsJSchedulingInternalExceptionEx(e); 
 				ex.log(m_logger);
+				operatorLog.warning("Cannot destroy %s - Scheduling internal error", arrayName);
 			}
     	}
     	activeArrays = null;
@@ -172,21 +213,28 @@ public class MasterImpl implements ComponentLifecycle,
 			arrayName = createNewControlArray(antennaIdList, photonicsList, 
 					corrType, schedulingMode);
 			if (arrayName == null){
+				operatorLog.warning("Cannot create array - %s component is not OPERATIONAL",
+						             Constants.CONTROL_MASTER_URL);
 				AcsJControlInternalExceptionEx ex = 
 					new AcsJControlInternalExceptionEx();
 				ex.setProperty("reason", Constants.CONTROL_MASTER_URL + " component is not OPERATIONAL");
 				ex.log(m_logger);
 				throw ex.toControlInternalExceptionEx();
 			}
+			operatorLog.info("Created CONTROL component for %s",
+						     arrayName);
 		} catch (InaccessibleException e) {
+			operatorLog.severe("Cannot create CONTROL component for array - CONTROL internal error");
 			AcsJControlInternalExceptionEx ex = new AcsJControlInternalExceptionEx(e);
 			ex.log(m_logger);
 			throw ex.toControlInternalExceptionEx();
 		} catch (InvalidRequest e) {
+			operatorLog.severe("Cannot create CONTROL component for array - CONTROL internal error");
 			AcsJControlInternalExceptionEx ex = new AcsJControlInternalExceptionEx(e);
 			ex.log(m_logger);
 			throw ex.toControlInternalExceptionEx();
 		} catch(org.omg.CORBA.SystemException e) {
+			operatorLog.severe("Cannot create CONTROL component for array - system error");
 			AcsJControlInternalExceptionEx ex = new AcsJControlInternalExceptionEx(e);
 			ex.log(m_logger);
 			throw ex.toControlInternalExceptionEx();
@@ -197,11 +245,15 @@ public class MasterImpl implements ComponentLifecycle,
 					                 schedulingMode,
 					                 lifecycleType,
 					                 policyName);
+			operatorLog.info("Created SCHEDULING component for %s",
+				     arrayName);
 		} catch (AcsJContainerServicesEx e) {
+			operatorLog.severe("Cannot create CONTROL component for array - ACS internal error");
 			AcsJACSInternalExceptionEx ex = new AcsJACSInternalExceptionEx(e);
 			ex.log(m_logger);
 			throw ex.toACSInternalExceptionEx();
 		} catch (AcsJSchedulingInternalExceptionEx e) {
+			operatorLog.severe("Cannot create CONTROL component for array - SCHEDULING internal error");
 			e.log(m_logger);
 			throw e.toSchedulingInternalExceptionEx();
 		}
@@ -234,24 +286,29 @@ public class MasterImpl implements ComponentLifecycle,
 		for (String key: toBeDeleted)
 			callbacks.remove(key);
 		
+		operatorLog.info("Created %s", arrayName);
+
 		return arrayInfo;
 	}
 
 	@Override
 	public void destroyArray(String arrayName, String name, String role) throws ACSInternalExceptionEx,
-	ControlInternalExceptionEx, SchedulingInternalExceptionEx{
+			ControlInternalExceptionEx, SchedulingInternalExceptionEx{
 		Object obj = null;
 		String schedArrayName = null;
+		operatorLog.info("Destroying %s", arrayName);
 		m_logger.info("About to destroy array: " + arrayName);
 		Array array = null;
 		try {
 			schedArrayName = NameTranslator.arrayToComponentName(arrayName);
 			obj = m_containerServices.getComponent(schedArrayName);
 		} catch (AcsJContainerServicesEx e) {
+			operatorLog.warning("Cannot destroy %s - ACS internal error", arrayName);
 			AcsJACSInternalExceptionEx ex = new AcsJACSInternalExceptionEx(e);
 			ex.log(m_logger);
 			throw ex.toACSInternalExceptionEx();
 		} catch (TranslationException e) {
+			operatorLog.warning("Cannot destroy %s - Scheduling internal error", arrayName);
 			AcsJSchedulingInternalExceptionEx ex = new AcsJSchedulingInternalExceptionEx(e);
 			ex.log(m_logger);
 			throw ex.toSchedulingInternalExceptionEx();
@@ -259,6 +316,15 @@ public class MasterImpl implements ComponentLifecycle,
 		
 		array = ArrayHelper.narrow(obj);
 		m_logger.info("Stopping SchedBlock in " + schedArrayName);
+		if (array.hasRunningSchedBlock()) {
+			try {
+				operatorLog.info("Stopping SchedBlock %s on %s",
+						array.getRunningSchedBlock(),
+						arrayName);
+			} catch (NoRunningSchedBlockEx e) {
+				// Quietly ignore it as we've just checked for one...
+			}
+		}
 		array.stop("Master Panel", "Master Panel");
 		array.stopRunningSchedBlock("Master Panel", "Master Panel");
 		array.destroyArray(name, role);
@@ -267,6 +333,7 @@ public class MasterImpl implements ComponentLifecycle,
 		array = null;
 		m_logger.info ("Releasing Scheduling Array " + schedArrayName);
 		m_containerServices.releaseComponent(schedArrayName);
+		operatorLog.info("Releasing Scheduling component %s", schedArrayName);
 		
 		//Notify to the callbacks
 		ArrayList<String> toBeDeleted =  new ArrayList<String>();
@@ -289,11 +356,14 @@ public class MasterImpl implements ComponentLifecycle,
 			m_logger.finest("About to release CONTROL Array");
 			controlMaster.destroyArray(arrayName);
 			m_logger.finest("Released CONTROL Array");
+			operatorLog.info("Released Control component for %s", arrayName);
 		} catch (InaccessibleException e) {
+			operatorLog.warning("Cannot release CONTROL component for %s - CONTROL internal error", arrayName);
 			AcsJControlInternalExceptionEx ex = new AcsJControlInternalExceptionEx(e);
 			ex.log(m_logger);
 			throw ex.toControlInternalExceptionEx();
 		} catch (InvalidRequest e) {
+			operatorLog.warning("Cannot release CONTROL component for %s - CONTROL internal error", arrayName);
 			AcsJControlInternalExceptionEx ex = new AcsJControlInternalExceptionEx(e);
 			ex.log(m_logger);
 			ex.toControlInternalExceptionEx();
@@ -443,7 +513,7 @@ public class MasterImpl implements ComponentLifecycle,
 		controlMaster = controlComp;
 	}
 	
-	void setLogger(Logger logger) {
+	void setLogger(AcsLogger logger) {
 		m_logger = logger;
 	}
 	
@@ -481,4 +551,113 @@ public class MasterImpl implements ComponentLifecycle,
 		DynamicSchedulingPolicyFactory.getInstance().createDSAPolicyBeans(xmlString);
 	}
 	
+	
+	public static void main(String args[]) {
+		final FakeAudienceFlogger operatorLog =
+			new FakeAudienceFlogger("Test", Audience.OPERATOR);
+		final String arrayName = "Array666";
+		String schedArrayName;
+		
+		try {
+			schedArrayName = NameTranslator.arrayToComponentName(arrayName);
+		} catch (TranslationException e) {
+			schedArrayName = e.getMessage();
+		}
+		final Array  array = new Array(){
+			/**  */
+			private static final long serialVersionUID = 1L;
+			@Override public void configure(String arrayName, ArraySchedulerMode[] modes, ArraySchedulerLifecycleType lifecycleType) {}
+			@Override public void configureDynamicScheduler(String policyName) {}
+			@Override public ArraySchedulerMode[] getModes() {return null;}
+			@Override public ArraySchedulerLifecycleType getLifecycleType() {return null;}
+			@Override public String getArrayName() {return null;}
+			@Override public String name() {return null;}
+			@Override public ComponentStates componentState() {return null;}
+			@Override public void addMonitorQueue(String arg0, SchedBlockQueueCallback arg1) {}
+			@Override public void delete(SchedBlockQueueItem arg0) {}
+			@Override public SchedBlockQueueItem[] getQueue() {return null;}
+			@Override public int getQueueCapacity() {return 0;}
+			@Override public SchedBlockQueueItem[] getExecutedQueue() {return null;}
+			@Override public void moveDown(SchedBlockQueueItem arg0) {}
+			@Override public void moveUp(SchedBlockQueueItem arg0) {}
+			@Override public SchedBlockQueueItem pull() {return null;}
+			@Override public void push(SchedBlockQueueItem arg0) {}
+			@Override public void removeMonitorQueue(String monitorName) {}
+			@Override public void start(String name, String role) {}
+			@Override public void stop(String name, String role) {}
+			@Override public String getRunningSchedBlock() throws NoRunningSchedBlockEx {return "uid://A666//X0/Xfake";}
+			@Override public boolean hasRunningSchedBlock() {return true;}
+			@Override public void stopRunningSchedBlock(String name, String role) {}
+			@Override public void addMonitorExecution(String monitorName, SchedBlockExecutionCallback callback) {}
+			@Override public void removeMonitorExecution(String monitorName) {}
+			@Override public void addMonitorGUI(String monitorName, ArrayGUICallback callback) {}
+			@Override public void removeMonitorGUI(String monitorName) {}
+			@Override public boolean isRunning() {return false;}
+			@Override public boolean isManual() {return false;}
+			@Override public boolean isFullAuto() {return false;}
+			@Override public void setFullAuto(boolean on, String name, String role) {}
+			@Override public void destroyArray(String name, String role) {}
+			@Override public SchedBlockExecutionItem[] getExecutions() {return null;}
+			@Override public IDLEntityRef startManualModeSession(String sbid) throws InvalidOperationEx {return null;}
+			@Override public boolean _is_a(String repositoryIdentifier) {return false;}
+			@Override public boolean _is_equivalent(Object other) {return false;}
+			@Override public boolean _non_existent() {return false;}
+			@Override public int _hash(int maximum) {return 0;}
+			@Override public Object _duplicate() {return null;}
+			@Override public void _release() {}
+			@Override public Object _get_interface_def() {return null;}
+			@Override public Request _request(String operation) {return null;}
+			@Override public Request _create_request(Context ctx, String operation, NVList arg_list, NamedValue result) {return null;}
+			@Override public Request _create_request(Context ctx, String operation, NVList arg_list, NamedValue result, ExceptionList exclist, ContextList ctxlist) {return null;}
+			@Override public Policy _get_policy(int policy_type) {return null;}
+			@Override public DomainManager[] _get_domain_managers() {return null;}
+			@Override public Object _set_policy_override(Policy[] policies, SetOverrideType set_add) {return null;}
+		};
+		
+		// initialize()
+		operatorLog.info("Connected to Weather Station Controller Component");
+		operatorLog.warning("Unable to retrieve Weather Station Controller Component %s", Constants.WEATHER_STATION_IF);
+		operatorLog.warning("Unable to retrieve Weather Station Controller Component %s", Constants.WEATHER_STATION_IF);
+		
+		// cleanUp()
+		operatorLog.warning("Automatically destroying %s at cleanUp. " +
+				"You should destroy the arrays before component cleanUp", arrayName);
+		operatorLog.info("%s destroyed", arrayName);
+		operatorLog.warning("Cannot destroy %s - ACS internal error", arrayName);
+		operatorLog.warning("Cannot destroy %s - Control subsystem internal error", arrayName);
+		operatorLog.warning("Cannot destroy %s - Scheduling internal error", arrayName);
+		
+		// createArray()
+		operatorLog.warning("Cannot create array - %s component is not OPERATIONAL",
+				Constants.CONTROL_MASTER_URL);
+		operatorLog.info("Created CONTROL component for %s",
+				arrayName);
+		operatorLog.severe("Cannot create CONTROL component for array - CONTROL internal error");
+		operatorLog.severe("Cannot create CONTROL component for array - CONTROL internal error");
+		operatorLog.severe("Cannot create CONTROL component for array - system error");
+		operatorLog.info("Created SCHEDULING component for %s",
+				arrayName);
+		operatorLog.severe("Cannot create CONTROL component for array - ACS internal error");
+		operatorLog.severe("Cannot create CONTROL component for array - SCHEDULING internal error");
+		operatorLog.info("Created %s", arrayName);
+
+		// destroyArray()
+		operatorLog.info("Destroying %s", arrayName);
+		operatorLog.warning("Cannot destroy %s - ACS internal error", arrayName);
+		operatorLog.warning("Cannot destroy %s - Scheduling internal error", arrayName);
+		try {
+			operatorLog.info("Stopping SchedBlock %s on %s",
+					array.getRunningSchedBlock(),
+					arrayName);
+		} catch (NoRunningSchedBlockEx e) {
+			operatorLog.info("Stopping SchedBlock %s on %s",
+					">> IT WENT WRONG <<",
+					arrayName);
+		}
+		operatorLog.info("Releasing Scheduling component %s", schedArrayName);
+		operatorLog.info("Released Control component for %s", arrayName);
+		operatorLog.warning("Cannot release CONTROL component for %s - CONTROL internal error", arrayName);
+		operatorLog.warning("Cannot release CONTROL component for %s - CONTROL internal error", arrayName);
+
+	}
 }
