30faeacf658f9275bbf8b88aac2d8122efffb40c
dclarke <dclarke@523d945c-050c-4681-91ec-863ad3bb968a>
2011-08-05 21:37:29 +0000

    Added progress messages
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@159759 523d945c-050c-4681-91ec-863ad3bb968a

--- IntTest/test/scripts/populate.sh
+++ IntTest/test/scripts/populate.sh
@@ -20,8 +20,13 @@ fi
 
 export JAVA_OPTIONS="-Dot.submission.host=http://127.0.0.1:8180 -Dot.submission.user=john -Dot.submission.pass=john -Dalma.obsprep.allowprivileged=true"
 
+declare -i i=0
+declare -i r
+
 for project in ${projects[@]} ; do
     ALMA-OT -batch ${script} ${project}
+    i=i+1
+    echo "$i / ${#projects[@]}"
 done
 
 unset JAVA_OPTIONS
