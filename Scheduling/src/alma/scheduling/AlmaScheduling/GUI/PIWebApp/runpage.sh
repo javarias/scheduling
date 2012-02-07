9a27d3b09deb6740a21780e88ee8012f06f32811
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2012-02-07 20:48:35 +0000

    Removed old scheduling classes (scheduler after the refactoring)
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@169220 523d945c-050c-4681-91ec-863ad3bb968a

--- Scheduling/src/alma/scheduling/AlmaScheduling/GUI/PIWebApp/runpage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#/bin/bash /export/home/frank/wlin/workspace/Scheduling/Scheduling/src/make clean all install
-
-#jarsigner -storepass nraoalma -keypass nraoalma  /export/home/frank/wlin/workspace/Scheduling/Scheduling/lib/schedulingComp.jar ALMACert 
-mozilla /export/home/frank/wlin/workspace/Scheduling/Scheduling/src/alma/scheduling/AlmaScheduling/GUI/PIWebApp/index.html
