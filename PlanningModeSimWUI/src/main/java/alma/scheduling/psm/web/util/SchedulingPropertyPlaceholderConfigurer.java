875645d4f8fec02a61174c625dade34d2ce48ebc
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2012-02-15 22:39:37 +0000

    Added custom place holder configurator for simulator web ui
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@169535 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ PlanningModeSimWUI/src/main/java/alma/scheduling/psm/web/util/SchedulingPropertyPlaceholderConfigurer.java
@@ -0,0 +1,68 @@
+/* ALMA - Atacama Large Millimiter Array
+ * (c) Associated Universities Inc., 2006 
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ */
+package alma.scheduling.psm.web.util;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+public class SchedulingPropertyPlaceholderConfigurer extends
+		PropertyPlaceholderConfigurer {
+
+	private static Logger logger = LoggerFactory.getLogger(SchedulingPropertyPlaceholderConfigurer.class);
+	private static final String WORKDIR_ENV_NAME = "APRC_WORK_DIR";
+	private static final String CONFIG_PROPS_FILENAME = "alma.scheduling.properties";
+	private Resource location = null;
+	
+	public SchedulingPropertyPlaceholderConfigurer() {
+		super();
+		String fileToCheck = null;
+		String workDirPath = System.getenv(WORKDIR_ENV_NAME);
+		String props_file = System.getProperty(CONFIG_PROPS_FILENAME);
+		if (props_file != null) 
+			fileToCheck = props_file;
+		else
+			if (workDirPath != null) 
+				fileToCheck = workDirPath + "/scheduling.properties";
+		if (fileToCheck == null) {
+			logger.warn("Using default default scheduling policies set on webContextfile inside the web application");
+			return;
+		}
+		File f = new File(fileToCheck);
+		if(f.exists() && f.isFile() && f.canRead()) {
+			logger.info("Using " + f.getAbsolutePath() + " as scheduling properties.");
+			location = new FileSystemResource(f);
+		} else {
+			logger.warn("Using default default scheduling policies set on webContextfile inside the web application. " +
+					"Property file " +f.getAbsolutePath() + " cannot be accessed.");
+		}
+	}
+	
+	@Override
+	public void setLocation(Resource location) {
+		if (this.location != null)
+			super.setLocation(this.location);
+		else
+			super.setLocation(location);
+	}
+	
+}
