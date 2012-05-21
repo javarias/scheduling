118153aed9078de509b0b7c7e4ee11efa8ae8604
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2012-05-21 23:21:14 +0000

    Enhanced ObsProjectDataLoader to include the ability to select between XML ObsProject DAO and Archive ObsProjectDao
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@174267 523d945c-050c-4681-91ec-863ad3bb968a

--- DSA/src/alma/scheduling/dataload/obsproject/ObsProjectDataLoader.java
+++ DSA/src/alma/scheduling/dataload/obsproject/ObsProjectDataLoader.java
@@ -1,114 +1,31 @@
-/*
- * ALMA - Atacama Large Millimeter Array
- * (c) European Southern Observatory, 2002
- * (c) Associated Universities Inc., 2002
- * Copyright by ESO (in the framework of the ALMA collaboration),
- * Copyright by AUI (in the framework of the ALMA collaboration),
- * All rights reserved.
- *
+/* ALMA - Atacama Large Millimiter Array
+ * (c) Associated Universities Inc., 2006 
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free software Foundation; either
+ * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- *
+ * 
  * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY, without even the implied warranty of
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307  USA
- *
- * "@(#) $Id: ObsProjectDataLoader.java,v 1.7 2012/04/25 22:25:01 javarias Exp $"
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
  */
 package alma.scheduling.dataload.obsproject;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.transaction.annotation.Transactional;
-
 import alma.scheduling.dataload.DataLoader;
-import alma.scheduling.datamodel.DAOException;
-import alma.scheduling.datamodel.obsproject.FieldSource;
-import alma.scheduling.datamodel.obsproject.ObsProject;
-import alma.scheduling.datamodel.obsproject.ObsUnit;
-import alma.scheduling.datamodel.obsproject.ObservingParameters;
-import alma.scheduling.datamodel.obsproject.dao.ObsProjectDao;
 import alma.scheduling.datamodel.obsproject.dao.ProjectDao;
-import alma.scheduling.datamodel.obsproject.dao.XmlObsProjectDao;
-import alma.scheduling.utils.SchedulingProperties;
 
-@Transactional
-public class ObsProjectDataLoader implements DataLoader {
-    
-	private static Logger logger = LoggerFactory.getLogger(ObsProjectDataLoader.class);
-	
+public interface ObsProjectDataLoader extends DataLoader {
+
 	/**
-	 * DAO to get Light Projects from files.
+	 * 
+	 * @param archProjectDao the Archive project DAO to set, it could be null
 	 */
-    XmlObsProjectDao xmlDao;
-    public void setXmlDao(XmlObsProjectDao xmlDao) {
-        this.xmlDao = xmlDao;
-    }
-
-    /**
-     * DAO to get projects from ARCHIVE XML Store.
-     */
-    ProjectDao archProjectDao;
-    public void setArchProjectDao(ProjectDao archProjectDao) {
-    	this.archProjectDao  = archProjectDao;
-    }
-    
-    /**
-     * DAO to store projects in work database.
-     */
-    ObsProjectDao dao;
-    public void setDao(ObsProjectDao dao) {
-        this.dao = dao;
-    }
-
-    @Override
-    @Transactional
-    public void load() {
-    	List<ObsProject> projects = null;
-    	if (xmlDao != null && SchedulingProperties.isPMSUsingXMLProjects()) {
-    		projects = xmlDao.getAllObsProjects();
-    	} else if (archProjectDao != null) {
-    		try {
-				projects = archProjectDao.getAllObsProjects();
-				System.out.println("Project Size: " + projects.size());
-			} catch (DAOException ex) {
-				logger.error("error getting projects from XML Store");
-				ex.printStackTrace();
-			}
-    	} else {
-    		logger.error("no DAO to retrieve projects has been setup");
-    		return;
-    	}
-//    	for(ObsProject p: projects){
-//    	    dao.saveOrUpdate(p);
-//    	}
-    	try{
-    		dao.saveOrUpdate(projects);
-    	}catch(NullPointerException e){
-    		logger.error("List of project to save or update has " + projects.size() + " elements.");
-    		if( projects.size() == 0 )
-    			logger.error("No obs projects retrived. Please check your sources.");
-    		else e.printStackTrace();
-    	}
-    }
-
-    @Override
-    public void clear() {
-        // not very efficient, replace later for a delete SQL command in the DAO
-        dao.deleteAll(dao.findAll(ObsProject.class));
-        dao.deleteAll(dao.findAll(ObsUnit.class));
-        dao.deleteAll(dao.findAll(ObservingParameters.class));
-        dao.deleteAll(dao.findAll(FieldSource.class));
-    }
-
+	public void setArchProjectDao(ProjectDao archProjectDao);
+	
 }
