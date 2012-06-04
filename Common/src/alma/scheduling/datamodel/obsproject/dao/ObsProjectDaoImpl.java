12506f895ad70938f6d6a221c638c8af709361b6
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2012-06-04 22:41:05 +0000

    Removed requirement for propagation as new in the delete all method
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@174909 523d945c-050c-4681-91ec-863ad3bb968a

--- Common/src/alma/scheduling/datamodel/obsproject/dao/ObsProjectDaoImpl.java
+++ Common/src/alma/scheduling/datamodel/obsproject/dao/ObsProjectDaoImpl.java
@@ -188,7 +188,7 @@ public class ObsProjectDaoImpl extends GenericDaoImpl implements ObsProjectDao, 
 
 
 	@Override
-	@Transactional(readOnly=false, isolation=Isolation.SERIALIZABLE)
+	@Transactional(readOnly=false)
 	public  synchronized void deleteAll() {
 		getSession().createQuery("delete from " + ObsProject.class.getCanonicalName()).executeUpdate();
 		getSession().createQuery("delete from " + ObsUnit.class.getCanonicalName()).executeUpdate();
