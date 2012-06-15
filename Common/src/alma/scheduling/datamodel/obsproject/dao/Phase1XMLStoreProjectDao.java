16d39fce40ad2afe33811912355cba3330b41b27
dclarke <dclarke@523d945c-050c-4681-91ec-863ad3bb968a>
2012-06-15 19:53:39 +0000

    Determine whether to get Phase 1 SBs from the ObsReview or the ObsProposal as determined by (a) the scheduling.phase1SBsource property and (b) whether there are SBs in the designated source.
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@175480 523d945c-050c-4681-91ec-863ad3bb968a

--- Common/src/alma/scheduling/datamodel/obsproject/dao/Phase1XMLStoreProjectDao.java
+++ Common/src/alma/scheduling/datamodel/obsproject/dao/Phase1XMLStoreProjectDao.java
@@ -33,6 +33,8 @@ import org.omg.CORBA.UserException;
 import alma.acs.entityutil.EntityException;
 import alma.entity.xmlbinding.obsproject.ObsUnitSetT;
 import alma.scheduling.datamodel.obsproject.ObsProject;
+import alma.scheduling.utils.SchedulingProperties;
+import alma.scheduling.utils.SchedulingProperties.Phase1SBSourceValue;
 import alma.xmlstore.ArchiveInternalError;
 import alma.xmlstore.Cursor;
 import alma.xmlstore.CursorPackage.QueryResult;
@@ -50,9 +52,18 @@ public class Phase1XMLStoreProjectDao extends AbstractXMLStoreProjectDao {
 //    	alma.entity.xmlbinding.valuetypes.types.StatusTStateType.READY.toString(),              
 //    	alma.entity.xmlbinding.valuetypes.types.StatusTStateType.PARTIALLYOBSERVED.toString()               
     };
+    
+    private Phase1SBSourceValue sbLocation;
 
 	public Phase1XMLStoreProjectDao() throws Exception {
 		super(Phase1XMLStoreProjectDao.class.getSimpleName());
+		
+		// The call to getPhase1SBSource() will throw an InvalidPropertyValueException
+		// if the user has specified an invalid value for the property. Thus, the rest
+		// of the code for importing Phase 1 projects from the XMLStore can assume that
+		// the flag is set correctly.
+		sbLocation = SchedulingProperties.getPhase1SBSource();
+		logger.info("Source for Phase 1 Projects' SchedBlocks will be " + sbLocation);
 	}
 
 	@Override
@@ -74,14 +85,18 @@ public class Phase1XMLStoreProjectDao extends AbstractXMLStoreProjectDao {
 	 * @param archive
 	 * @param apdmProposals
 	 */
-	protected void getAPDMProjectsFor(
+	protected List<alma.entity.xmlbinding.obsproject.ObsProject> getAPDMProjectsFor(
 			ArchiveInterface                                     archive,
 			List<alma.entity.xmlbinding.obsproposal.ObsProposal> apdmProposals) {
+		
+		List<alma.entity.xmlbinding.obsproject.ObsProject> result
+					= new Vector<alma.entity.xmlbinding.obsproject.ObsProject>();
+		
 		for (alma.entity.xmlbinding.obsproposal.ObsProposal apdmProposal : apdmProposals ) {
 			alma.entity.xmlbinding.obsproject.ObsProjectRefT projectRef = apdmProposal.getObsProjectRef();
 			String                                           projectId = projectRef.getEntityId();
 			try {
-				archive.getObsProject(projectId);
+				result.add(archive.getObsProject(projectId));
 				logger.info(String.format(
 						"Succesfully got APDM ObsProject %s for APDM ObsProposal %s",
 						projectId,
@@ -98,6 +113,103 @@ public class Phase1XMLStoreProjectDao extends AbstractXMLStoreProjectDao {
 						projectId,
 						apdmProposal.getObsProposalEntity().getEntityId(),
 						e.getMessage()));
+			}
+		}
+		
+		return result;
+	}
+
+	/**
+	 * Make sure that all the APDM ObsReviews corresponding to the
+	 * given APDM ObsProjects are in the given ArchiveInterface's
+	 * cache.
+	 * 
+	 * @param archive
+	 * @param apdmProjects
+	 */
+	protected void getAPDMReviewsFor(
+			ArchiveInterface                                   archive,
+			List<alma.entity.xmlbinding.obsproject.ObsProject> apdmProjects) {
+		for (alma.entity.xmlbinding.obsproject.ObsProject apdmProject : apdmProjects ) {
+			alma.entity.xmlbinding.obsreview.ObsReviewRefT reviewRef = apdmProject.getObsReviewRef();
+			if (reviewRef != null) {
+				String reviewId = reviewRef.getEntityId();
+				try {
+					archive.getObsReview(reviewId);
+					logger.info(String.format(
+							"Succesfully got APDM ObsReview %s for APDM ObsProject %s",
+							reviewId,
+							apdmProject.getObsProjectEntity().getEntityId()));
+				} catch (EntityException e) {
+					logger.warning(String.format(
+							"Cannot get APDM ObsReview %s for APDM ObsProject %s - %s",
+							reviewId,
+							apdmProject.getObsProjectEntity().getEntityId(),
+							e.getMessage()));
+				} catch (UserException e) {
+					logger.warning(String.format(
+							"Cannot get APDM ObsReview %s for APDM ObsProject %s - %s",
+							reviewId,
+							apdmProject.getObsProjectEntity().getEntityId(),
+							e.getMessage()));
+				}
+			} else {
+				logger.warning(String.format(
+						"APDM ObsProject %s has no APDM ObsReview",
+						apdmProject.getObsProjectEntity().getEntityId()));
+			}
+		}
+	}
+
+	/**
+	 * Make sure that where necessary the APDM ObsReviews corresponding
+	 * to the given APDM ObsProjects are in the cache of the given
+	 * ArchiveInterface's. "Where necessary" means, basically, when we
+	 * have been told to look in the ObsProposal first, but that there
+	 * are no references to any SchedBlocks in the ObsProposal.
+	 * 
+	 * @param archive
+	 * @param apdmProjects
+	 */
+	protected void getNecessaryAPDMReviewsFor(
+			ArchiveInterface                                   archive,
+			List<alma.entity.xmlbinding.obsproject.ObsProject> apdmProjects) {
+		for (alma.entity.xmlbinding.obsproject.ObsProject apdmProject : apdmProjects ) {
+			
+			alma.entity.xmlbinding.obsproposal.ObsProposalRefT proposalRef = apdmProject.getObsProposalRef();
+			alma.entity.xmlbinding.obsproposal.ObsProposal     apdmProposal = archive.cachedObsProposal(proposalRef.getEntityId());
+			
+			if (apdmProposal == null || !hasSchedBlocks(apdmProposal.getObsPlan())) {
+				// Either there isn't an ObsProposal, or it has no SchedBlocks, ergo
+				// get the ObsReview.
+				alma.entity.xmlbinding.obsreview.ObsReviewRefT reviewRef = apdmProject.getObsReviewRef();
+				if (reviewRef != null) {
+					String reviewId = reviewRef.getEntityId();
+					
+					try {
+						archive.getObsReview(reviewId);
+						logger.info(String.format(
+								"Succesfully got APDM ObsReview %s for APDM ObsProject %s",
+								reviewId,
+								apdmProject.getObsProjectEntity().getEntityId()));
+					} catch (EntityException e) {
+						logger.warning(String.format(
+								"Cannot get APDM ObsReview %s for APDM ObsProject %s - %s",
+								reviewId,
+								apdmProject.getObsProjectEntity().getEntityId(),
+								e.getMessage()));
+					} catch (UserException e) {
+						logger.warning(String.format(
+								"Cannot get APDM ObsReview %s for APDM ObsProject %s - %s",
+								reviewId,
+								apdmProject.getObsProjectEntity().getEntityId(),
+								e.getMessage()));
+					}
+				} else {
+					logger.warning(String.format(
+							"APDM ObsProject %s has no APDM ObsReview",
+							apdmProject.getObsProjectEntity().getEntityId()));
+				}
 			}
 		}
 	}
@@ -135,12 +247,13 @@ public class Phase1XMLStoreProjectDao extends AbstractXMLStoreProjectDao {
 //		String query = new String("/prp:ObsProposal");
 		String query = makeQuery(OPPhase1RunnableStates);
 		String schema = new String("ObsProposal");
+		Cursor cursor = null;
 		
 		logger.info(String.format(
 				"Getting interesting proposals, query = %s, schema = %s",
 				query, schema));
 		try {
-			Cursor cursor = xmlStore.query(query, schema);
+			cursor = xmlStore.query(query, schema);
 			if (cursor == null) {
 				logger.severe(String.format(
 						"Cannot get APDM ObsProposals - cursor returned by query is null"));
@@ -168,37 +281,167 @@ public class Phase1XMLStoreProjectDao extends AbstractXMLStoreProjectDao {
 								e.getMessage()));
 					}
 				}
-				cursor.close();
 			}
 		} catch(ArchiveInternalError e) {
 			logger.severe(String.format(
 					"Cannot get APDM ObsProposals - %s",
 					e.getMessage()));
+		} finally {
+			if (cursor != null) {
+				cursor.close();
+				cursor = null;
+			}
 		}
 		return result;
 	}
 
+	/* (non-Javadoc)
+	 * @see alma.scheduling.datamodel.obsproject.dao.AbstractXMLStoreProjectDao#getInterestingProjects(alma.scheduling.datamodel.obsproject.dao.ArchiveInterface)
+	 */
 	@Override
 	protected void getInterestingProjects(ArchiveInterface archive) {
+		
 		List<alma.entity.xmlbinding.obsproposal.ObsProposal> apdmProposals;
+		List<alma.entity.xmlbinding.obsproject.ObsProject>   apdmProjects;
+		
 		Date start = new Date();
 		apdmProposals = fetchInterestingAPDMProposals();
 		Date end = new Date();
-		logger.info("Fetching apdm proposals from archive took " + (end.getTime() - start.getTime()) + " ms");
+		logger.info("Fetching APDM Proposals from archive took " + (end.getTime() - start.getTime()) + " ms");
+		
 		start = new Date();
-		getAPDMProjectsFor(archive, apdmProposals);
+		apdmProjects = getAPDMProjectsFor(archive, apdmProposals);
 		end = new Date();
-		logger.info("Getting apdm project from archive took " + (end.getTime() - start.getTime()) + " ms");
+		logger.info("Getting APDM Projects from archive took " + (end.getTime() - start.getTime()) + " ms");
+		
+		switch (sbLocation) {
+			case REVIEW_ONLY:
+			case REVIEW_THEN_PROPOSAL:
+				// In these cases we have to get the ObsReview.
+				start = new Date();
+				getAPDMReviewsFor(archive, apdmProjects);
+				end = new Date();
+				logger.info("Getting APDM Reviews from archive took " + (end.getTime() - start.getTime()) + " ms");
+				if (sbLocation == Phase1SBSourceValue.REVIEW_ONLY) {
+				    setAllLocations(apdmProjects, sbLocation);
+				} else {
+					setConditionalLocations(apdmProjects, sbLocation);
+				}
+				break;
+			case PROPOSAL_ONLY:
+				// Never any need for the ObsReviews.
+				logger.info("No need to get APDM Reviews from archive");
+			    setAllLocations(apdmProjects, sbLocation);
+				break;
+			case PROPOSAL_THEN_REVIEW:
+				// In these cases we might have to get the ObsReview.
+				start = new Date();
+				getNecessaryAPDMReviewsFor(archive, apdmProjects); // Sets the locations
+				end = new Date();
+				logger.info("Getting necessary APDM Reviews from archive took " + (end.getTime() - start.getTime()) + " ms");
+				setConditionalLocations(apdmProjects, sbLocation);
+				break;
 		}
+	}
+
+	/**
+	 * @param apdmProjects
+	 * @param location
+	 */
+	private void setAllLocations(
+			List<alma.entity.xmlbinding.obsproject.ObsProject> apdmProjects,
+			Phase1SBSourceValue location) {
+		logger.info(String.format(
+				"Using APDM %ss as source for Phase 1 SchedBlocks for all APDM ObsProjects",
+				location));
+		for (alma.entity.xmlbinding.obsproject.ObsProject apdmProject : apdmProjects) {
+			archive.rememberPhase1Location(
+					apdmProject.getObsProjectEntity().getEntityId(),
+					location);
+		}
+	}
+
+	/**
+	 * @param apdmProjects
+	 * @param location
+	 */
+	private void setConditionalLocations(
+			List<alma.entity.xmlbinding.obsproject.ObsProject> apdmProjects,
+			Phase1SBSourceValue location) {
+		
+		for (alma.entity.xmlbinding.obsproject.ObsProject apdmProject : apdmProjects) {
+			
+			final String projectId = apdmProject.getObsProjectEntity().getEntityId();
+			final Phase1SBSourceValue resolvedLocation;
+			
+			switch (location) {
+				case REVIEW_THEN_PROPOSAL:
+					alma.entity.xmlbinding.obsreview.ObsReviewRefT ref = apdmProject.getObsReviewRef();
+					if (ref != null) {
+						alma.entity.xmlbinding.obsreview.ObsReview apdmReview = archive.cachedObsReview(ref.getEntityId());
+						if (apdmReview != null && hasSchedBlocks(apdmReview.getObsPlan())) {
+							resolvedLocation = Phase1SBSourceValue.REVIEW_ONLY;
+						} else {
+							resolvedLocation = Phase1SBSourceValue.PROPOSAL_ONLY;
+						}
+					} else {
+						// No review found, use the proposal
+						resolvedLocation = Phase1SBSourceValue.PROPOSAL_ONLY;
+					}
+					break;
+				case PROPOSAL_THEN_REVIEW:
+					alma.entity.xmlbinding.obsproposal.ObsProposalRefT proposalRef = apdmProject.getObsProposalRef();
+					if (proposalRef != null) {
+						alma.entity.xmlbinding.obsproposal.ObsProposal apdmProposal = archive.cachedObsProposal(proposalRef.getEntityId());
+						if (apdmProposal != null && hasSchedBlocks(apdmProposal.getObsPlan())) {
+							resolvedLocation = Phase1SBSourceValue.PROPOSAL_ONLY;
+						} else {
+							resolvedLocation = Phase1SBSourceValue.REVIEW_ONLY;
+						}
+					} else {
+						// No proposal found, use the review
+						resolvedLocation = Phase1SBSourceValue.PROPOSAL_ONLY;
+					}
+					break;
+				default:
+					// This is a bit weird, shouldn't really get called with these
+					// arguments. However, just set the location accordingly.
+					resolvedLocation = location;
+					break;
+			}
+			
+			if (resolvedLocation == Phase1SBSourceValue.PROPOSAL_ONLY) {
+				logger.info(String.format(
+						"Using APDM ObsProposal %s as source for Phase 1 SchedBlocks for ObsProject %s",
+						apdmProject.getObsProposalRef().getEntityId(), projectId));
+			} else {
+				logger.info(String.format(
+						"Using APDM ObsReview %s as source for Phase 1 SchedBlocks for ObsProject %s",
+						apdmProject.getObsReviewRef().getEntityId(), projectId));
+			}
+			archive.rememberPhase1Location(projectId, resolvedLocation);
+		}
+	}
 
 	@Override
 	protected ObsUnitSetT getTopLevelOUSForProject(
 			ArchiveInterface                             archive,
 			alma.entity.xmlbinding.obsproject.ObsProject apdmProject) {
-		alma.entity.xmlbinding.obsproposal.ObsProposal proposal =
-			archive.cachedObsProposal(apdmProject.getObsProposalRef().getEntityId());
+
+		alma.entity.xmlbinding.obsproject.ObsPhaseT phase;
 		
-		return proposal.getObsPlan();
+		switch (archive.getPhase1Location(apdmProject.getObsProjectEntity().getEntityId())) {
+			case REVIEW_ONLY:
+				phase = archive.cachedObsReview(apdmProject.getObsReviewRef().getEntityId());
+				break; 
+			case PROPOSAL_ONLY:
+				phase = archive.cachedObsProposal(apdmProject.getObsProposalRef().getEntityId());
+				break; 
+			default:
+				return null;
+		}
+		
+		return phase.getObsPlan();
 	}
 
 	protected void getAPDMSchedBlocksFor(
@@ -276,7 +519,41 @@ public class Phase1XMLStoreProjectDao extends AbstractXMLStoreProjectDao {
 		return true;
 	}
 	
-	
+
+	/**
+	 * Look down the given ObsUnitSet for SchedBlock refs, return true
+	 * if you find some and false it you don't.
+	 *  
+	 * @param apdmOUS - the ObsUnitSet to search
+	 * @return <code>true</code> if you find a SchedBlockRefT and <code>false</code> if you do not.
+	 */
+	private boolean hasSchedBlocks(
+			alma.entity.xmlbinding.obsproject.ObsUnitSetT apdmOUS) {
+		
+		alma.entity.xmlbinding.obsproject.ObsUnitSetTChoice
+				choice = apdmOUS.getObsUnitSetTChoice();
+		
+		if (choice == null) {
+			// No children at all
+			return false;
+		}
+		
+		if (choice.getSchedBlockRefCount() > 0) {
+			// Oh look, SchedBlockRefs!
+			return true;
+		}
+
+		
+		for (alma.entity.xmlbinding.obsproject.ObsUnitSetT
+					childOUS : choice.getObsUnitSet()) {
+			if (hasSchedBlocks(childOUS)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
 	public static void main(String[] args) {
 		String query = makeQuery(OPPhase1RunnableStates);
 		System.out.println(query);
