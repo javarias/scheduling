a7be04d328aa2e658b6e0d599469a44c5ccdea11
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2012-09-12 21:23:18 +0000

    Hide by default the information pane of the interactive panel
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@179167 523d945c-050c-4681-91ec-863ad3bb968a

--- Array/src/alma/scheduling/array/guis/InteractivePanel.java
+++ Array/src/alma/scheduling/array/guis/InteractivePanel.java
@@ -93,7 +93,7 @@ import alma.statearchiveexceptions.wrappers.AcsJNullEntityIdEx;
 /**
  *
  * @author dclarke
- * $Id: InteractivePanel.java,v 1.34 2012/09/12 21:10:16 javarias Exp $
+ * $Id: InteractivePanel.java,v 1.35 2012/09/12 21:23:09 javarias Exp $
  */
 @SuppressWarnings("serial")
 public class InteractivePanel extends AbstractArrayPanel
@@ -579,10 +579,13 @@ public class InteractivePanel extends AbstractArrayPanel
 		
 		subSplit.setTopComponent(opPanel);
 		subSplit.setBottomComponent(sbPanel);
-		subSplit.setDividerLocation(0.0);
+		subSplit.setDividerLocation(0.5);
+		subSplit.setResizeWeight(0.5);
 		split.setTopComponent(subSplit);
 		split.setBottomComponent(details);
-//		split.setDividerLocation(1.0);
+		split.setDividerLocation(1.0);
+		split.setResizeWeight(1.0);
+		details.setSize(new Dimension(details.getWidth(), 0));
 		details.setPreferredSize(new Dimension(details.getWidth(), 0));
 		split.setOneTouchExpandable(true);
 		
