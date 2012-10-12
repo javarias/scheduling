508fa310835858daabe4df8ce994d735be0f655d
javarias <javarias@523d945c-050c-4681-91ec-863ad3bb968a>
2012-10-12 17:37:13 +0000

    Added dummy functionality for Calibrator check plugin and periodically checker. The full functionality will be available when the calibrartor check script will have been delivered
    git-svn-id: https://alma-svn.aoc.nrao.edu/p2/trunk/SCHEDULING@180423 523d945c-050c-4681-91ec-863ad3bb968a

---
+++ ArchiveUpdater/src/alma/scheduling/archiveupd/external/CalibratorCheckPluginImpl.java
@@ -0,0 +1,58 @@
+package alma.scheduling.archiveupd.external;
+
+import static alma.lifecycle.config.SpringConstants.STATE_SYSTEM_SPRING_CONFIG;
+
+import java.util.logging.Logger;
+
+
+import alma.entity.xmlbinding.sbstatus.SBStatusEntityT;
+import alma.entity.xmlbinding.schedblock.SchedBlock;
+import alma.entity.xmlbinding.valuetypes.types.StatusTStateType;
+import alma.lifecycle.config.StateSystemContextFactory;
+import alma.lifecycle.persistence.StateArchive;
+import alma.lifecycle.stateengine.StateEngine;
+import alma.lifecycle.stateengine.action.plugin.CalibratorCheckPlugin;
+import alma.lifecycle.stateengine.constants.Role;
+import alma.lifecycle.stateengine.constants.Subsystem;
+
+public class CalibratorCheckPluginImpl implements CalibratorCheckPlugin {
+
+	private static StateEngine stateEngine;
+	private static StateArchive stateArchive;
+	
+	public CalibratorCheckPluginImpl() {
+		synchronized (CalibratorCheckPluginImpl.class) {
+			if (!StateSystemContextFactory.INSTANCE.isInitialized()) {
+				StateSystemContextFactory.INSTANCE.init(STATE_SYSTEM_SPRING_CONFIG,
+								Logger.getAnonymousLogger());
+			}
+			if(stateArchive == null)
+				stateArchive = StateSystemContextFactory.INSTANCE.getStateArchive();
+			if(stateEngine == null)
+				stateEngine = StateSystemContextFactory.INSTANCE.getStateEngine();
+		}
+	}
+	
+	@Override
+	public void processSchedBlock(SchedBlock schedBlock) {
+		SBStatusEntityT sbStatus = new SBStatusEntityT();
+		sbStatus.setEntityId(schedBlock.getSBStatusRef().getEntityId());
+		try {
+			Logger.getAnonymousLogger().info("Tranistioning SB: " 
+					+ schedBlock.getSchedBlockEntity().getEntityId() + " to " + StatusTStateType.CALIBRATORCHECK);
+			stateEngine.changeState(sbStatus, StatusTStateType.CALIBRATORCHECK, Subsystem.SCHEDULING, Role.AOD);
+			//TODO: Call the script to check for available calibrations
+//			Process p = Runtime.getRuntime().exec("script");
+//			p.waitFor();
+//			if(p.exitValue() == 0)
+			Logger.getAnonymousLogger().info("Tranistioning SB: " 
+					+ schedBlock.getSchedBlockEntity().getEntityId() + " to " + StatusTStateType.READY);
+				stateEngine.changeState(sbStatus, StatusTStateType.READY, Subsystem.SCHEDULING, Role.AOD);
+		} catch (Exception e) {
+			Logger.getAnonymousLogger().severe("Failed to tranistion SB: " 
+					+ schedBlock.getSchedBlockEntity().getEntityId() + " Cause: " + e.getMessage());
+			e.printStackTrace();
+		} 
+	}
+
+}
---
+++ ArchiveUpdater/src/alma/scheduling/archiveupd/external/SchedBlockStatusChecker.java
@@ -0,0 +1,107 @@
+package alma.scheduling.archiveupd.external;
+
+import static alma.lifecycle.config.SpringConstants.STATE_SYSTEM_SPRING_CONFIG;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import alma.ACSErrTypeCommon.wrappers.AcsJIllegalArgumentEx;
+import alma.entity.xmlbinding.sbstatus.SBStatus;
+import alma.entity.xmlbinding.sbstatus.SBStatusEntityT;
+import alma.entity.xmlbinding.valuetypes.types.StatusTStateType;
+import alma.lifecycle.config.StateSystemContextFactory;
+import alma.lifecycle.persistence.StateArchive;
+import alma.lifecycle.stateengine.StateEngine;
+import alma.lifecycle.stateengine.constants.Role;
+import alma.lifecycle.stateengine.constants.Subsystem;
+import alma.statearchiveexceptions.wrappers.AcsJInappropriateEntityTypeEx;
+import alma.statearchiveexceptions.wrappers.AcsJNoSuchEntityEx;
+import alma.stateengineexceptions.wrappers.AcsJNoSuchTransitionEx;
+import alma.stateengineexceptions.wrappers.AcsJNotAuthorizedEx;
+import alma.stateengineexceptions.wrappers.AcsJPostconditionFailedEx;
+import alma.stateengineexceptions.wrappers.AcsJPreconditionFailedEx;
+
+public class SchedBlockStatusChecker {
+
+	private static StateEngine stateEngine;
+	private static StateArchive stateArchive;
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+	private static final long TIME_TO_SUSPEND = 60 * 24 * 60 * 60 * 1000; //60 days
+	private static final long TIME_TO_CHECK_FOR_CALIBRATIONS = 30 * 60 * 1000; //30 mins
+	
+	public SchedBlockStatusChecker() {
+		synchronized(SchedBlockStatusChecker.class) {
+			if (!StateSystemContextFactory.INSTANCE.isInitialized()) {
+				StateSystemContextFactory.INSTANCE.init(STATE_SYSTEM_SPRING_CONFIG,
+								Logger.getAnonymousLogger());
+				stateArchive = StateSystemContextFactory.INSTANCE.getStateArchive();
+				stateEngine = StateSystemContextFactory.INSTANCE.getStateEngine();
+			}
+		}
+	}
+	
+	public void checkForStatus() {
+		checkForCalibrations();
+	}
+	
+	public void forceCheckForStatus() {
+		forceCheckForCalibrations();
+	}
+	
+	private void checkForCalibrations() {
+		String[] states = {StatusTStateType.CALIBRATORCHECK.toString()};
+		Date currentDate = new Date();
+		try {
+			SBStatus[] statuses = stateArchive.findSBStatusByState(states);
+			for(SBStatus s: statuses) {
+				if(s.getTimeOfUpdate() != null) {
+					try {
+						Date date = dateFormat.parse(s.getTimeOfUpdate());
+						//check if the SB has been waiting for calibrations too long 
+						if (date.getTime() < (currentDate.getTime() - TIME_TO_SUSPEND)) {
+							stateEngine.changeState(s.getSBStatusEntity(),  StatusTStateType.SUSPENDED, Subsystem.SCHEDULING, Role.AOD);
+						}
+						//Check if last update of SBStatus was not too soon
+						else if(date.getTime() < (currentDate.getTime() - TIME_TO_CHECK_FOR_CALIBRATIONS)) { 
+							//TODO: Run script and check for calibrations
+						} 
+					} catch (AcsJNoSuchTransitionEx e) {
+						e.printStackTrace();
+					} catch (AcsJNotAuthorizedEx e) {
+						e.printStackTrace();
+					} catch (AcsJPreconditionFailedEx e) {
+						e.printStackTrace();
+					} catch (AcsJPostconditionFailedEx e) {
+						e.printStackTrace();
+					} catch (AcsJNoSuchEntityEx e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} catch (AcsJIllegalArgumentEx e) {
+			e.printStackTrace();
+		} catch (AcsJInappropriateEntityTypeEx e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void forceCheckForCalibrations() {
+		String[] states = { StatusTStateType.CALIBRATORCHECK.toString() };
+		SBStatus[] statuses;
+		try {
+			statuses = stateArchive.findSBStatusByState(states);
+			for (SBStatus s : statuses) {
+				// TODO: Run script and check for calibrations
+			}
+		} catch (AcsJIllegalArgumentEx e) {
+			e.printStackTrace();
+		} catch (AcsJInappropriateEntityTypeEx e) {
+			e.printStackTrace();
+		}
+
+	}
+}
