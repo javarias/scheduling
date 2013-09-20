78c405c58caa7201502952c60c3a1a8a0e524582
Jorge Avarias <javarias@nrao.edu>
2013-09-20 12:18:57 -0600

    Added the arrayType selector to the selectors list

---
+++ DSA/src/alma/scheduling/algorithm/observatory/ArrayTypeSelector.java
@@ -0,0 +1,41 @@
+package alma.scheduling.algorithm.observatory;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+
+import alma.scheduling.algorithm.sbselection.AbstractBaseSelector;
+import alma.scheduling.algorithm.sbselection.NoSbSelectedException;
+import alma.scheduling.datamodel.observatory.ArrayConfiguration;
+import alma.scheduling.datamodel.obsproject.SchedBlock;
+
+public class ArrayTypeSelector extends AbstractBaseSelector {
+
+	public ArrayTypeSelector(String selectorName) {
+		super(selectorName);
+	}
+
+	@Override
+	public Collection<SchedBlock> select(Date ut, ArrayConfiguration arrConf)
+			throws NoSbSelectedException {
+		throw new RuntimeException("Not implemented");
+	}
+
+	@Override
+	public Criterion getCriterion(Date ut, ArrayConfiguration arrConf) {
+		if (arrConf.getArrayType() == null)
+			return null;
+		Criterion retval = Restrictions.eq("obsUnitControl.arrayRequested", arrConf.getArrayType());
+		return retval;
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().toString();
+	}
+
+	
+	
+}
